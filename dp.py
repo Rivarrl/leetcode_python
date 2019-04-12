c2c719d40f40617cb5d9ec572c1890a3bebae3d0
rivarrl <tianbao.liu@iquantex.com>
2019-04-12 18:16:24 +0800

    add

--- dp.py
+++ dp.py
@@ -148,25 +148,131 @@ def lis_2(list_num):
     return dp.__len__()
 
 
+def lis_ex1(nums):
+    """
+    VIJOS1098
+    选合唱队成员
+    N位同学站成一排，音乐老师要请其中的(N-K)位同学出列，使得剩下的K位同学排成合唱队形
+    合唱队形是指这样的一种队形：设K位同学从左到右依次编号为1, 2, …, K，他们的身高分别为T1, T2, …, TK
+    则他们的身高满足T1 < T2 < … < Ti , Ti > Ti+1 > … > TK (1 <= i <= K)
+    你的任务是已知所有N位同学的身高，计算最少需要几位同学出列，可以使得剩下的同学排成合唱队形
+    例如: nums = [186, 186, 150, 200, 160, 130, 197, 220] res = 4 去掉186 186(或150) 197 220
+    思路: 将每个同学作为中心，分别去求其左侧的最长递增序列和右侧的最长递减序列，总人数 - (两侧相加 - 1)即为所得
+    :param nums: List[int]
+    :return: int
+    """
+    total = nums.__len__()
+    dph = [1 for _ in nums]
+    dpt = [1 for _ in nums]
+    for i in range(total):
+        ri = total - 1 - i
+        for j in range(i):
+            rj = total - 1 - j
+            if nums[i] > nums[j] and dph[i] <= dph[j]:
+                dph[i] = dph[j] + 1
+            if nums[ri] > nums[rj] and dpt[ri] <= dpt[rj]:
+                dpt[ri] = dpt[rj] + 1
+    res = 0
+    for i in range(total):
+        if dph[i] + dpt[i] > res:
+            res = dph[i] + dpt[i]
+    return total + 1 - res
+
+
+def max_sub_rec(nums):
+    """
+    最大子段和
+    给定n个整数组成的序列a1,a2,…,an，求该序列子段和的最大值。
+    最大子段和不能是负数，当序列中均为负数时定义最大子段和为0。
+    例如: [-2, 11, -4, 13, -5, -2]的最大子段([11, -4, 13])和为20。
+    思路1(递归): 首先可以想到用分治的方法解决这个问题，如果将给定的序列a[1…n]分成长度相等的两段a[1…n//2]和a[n//2+1…n]，分别求出这两段的最大子段和。
+        则该给定序列的最大子段和有三种情况：
+            1. 和a[1…n//2]的最大子段和相同；
+            2. 和a[n//2+1…n]的最大子段和相同；
+            3. 最大子段和包含两部分。
+        前两种情形我们可以用递归方法求出，第三种情形可以分别求出两部分的最大子段和值再相加。序列的最大子段和即为这三种情形的最大值。
+    :param nums: List[int]
+    :return: int
+    """
+    l = len(nums)
+
+    def inner(sn, l, r):
+        """
+        内部函数，递归用
+        :param sn: List[int]
+        :param l: int
+        :param r: int
+        :return: int
+        """
+        if l == r:
+            return sn[l]
+        mid = (l + r) // 2
+        s1 = inner(sn, l, mid)
+        s2 = inner(sn, mid + 1, r)
+        i, j = mid, mid + 1
+        s31 = sn[i]
+        s32 = sn[j]
+        while (i > l and s31 + sn[i - 1] > s31):
+            s31 += sn[i - 1]
+            i -= 1
+        while (j < r and s32 + sn[j + 1] > s32):
+            s32 += sn[j + 1]
+            j += 1
+        s3 = s31 + s32
+        return max(s1, s2, s3)
+
+    return inner(nums, 0, l - 1)
+
+
+def max_sub_dp(nums):
+    """
+    最大子段和
+    给定n个整数组成的序列a1,a2,…,an，求该序列子段和的最大值。
+    最大子段和不能是负数，当序列中均为负数时定义最大子段和为0。
+    例如: [-2, 11, -4, 13, -5, -2]的最大子段([11, -4, 13])和为20。
+    思路2(动态规划): 利用一维数组dp，dp[i]表示从0到i的子串最大和(max(nums[j:i]) for j in (0, i))
+    :param nums: List[int]
+    :return: int
+    """
+    l = len(nums)
+    dp = [nums[0]]
+    dp.extend([0] * (l - 1))
+    for i in range(1, l):
+        dp[i] = dp[i - 1] + nums[i] if dp[i - 1] > 0 else nums[i]
+    return max(dp)
+
+
 if __name__ == '__main__':
-    # res1 = min_of_multiply([1, 3, 2, 4, 2])
+    # q1in1 = [1, 3, 2, 4, 2]
+    # res1 = min_of_multiply(q1in1)
     # print(res1)
 
-    # tri = [[7, 0, 0, 0, 0], [3, 8, 0, 0, 0], [8, 1, 0, 0, 0], [2, 7, 4, 4, 0], [4, 5, 2, 6, 5]]
-    # res2 = triangle_max_pace(tri, 2)
+    # q2in1 = [[7, 0, 0, 0, 0], [3, 8, 0, 0, 0], [8, 1, 0, 0, 0], [2, 7, 4, 4, 0], [4, 5, 2, 6, 5]]
+    # q2in2 = 2
+    # res2 = triangle_max_pace(q2in1, q2in2)
     # print(res2)
 
-    # s1 = "ABCBDAB"
-    # s2 = "BDCABA"
-    # res3 = lcs(s1, s2)
+    # q3in1 = "ABCBDAB"
+    # q3in2 = "BDCABA"
+    # res3 = lcs(q3in1, q3in2)
     # print(res3)
 
-    # s = "Ab3bd"
-    # res3_ex1 = palindrome(s)
-    # print(res3_ex1)
+    # q3_2in1 = "Ab3bd"
+    # res3_2 = palindrome(q4in1)
+    # print(res3_2)
 
-    q = [0, 3, 1, 2, 5, 9, 4, 14, 10, 12]
-    res4 = lis(q)
-    print(res4)
-    res4_2 = lis_2(q)
-    print(res4_2)
+    # q4in1 = [0, 3, 1, 2, 5, 9, 4, 14, 10, 12]
+    # res4 = lis(q4in1)
+    # print(res4)
+    # res4_2 = lis_2(q4in1)
+    # print(res4_2)
+
+    # q4ex1in1 = [186, 186, 150, 200, 160, 130, 197, 220]
+    # res4_ex1 = lis_ex1(q4ex1in1)
+    # print(res4_ex1)
+
+    q5in1 = [-2, 11, -4, 13, -5, -2]
+    res5 = max_sub_rec(q5in1)
+    print(res5)
+    res5_2 = max_sub_dp(q5in1)
+    print(res5_2)

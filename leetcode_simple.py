debb3ff8139c3b128cb5a4fdff852b54a26a9968
Rivarrl <1049793871@qq.com>
2019-05-04 00:47:31 +0800

    fix

--- leetcode_simple.py
+++ leetcode_simple.py
@@ -1254,10 +1254,35 @@ def getSum(a, b):
     pass
 
 
+def findLHS(nums):
+    """
+    594. 最长和谐子序列
+    和谐数组是指一个数组里元素的最大值和最小值之间的差别正好是1。
+    现在，给定一个整数数组，你需要在所有可能的子序列中找到最长的和谐子序列的长度。
+    示例 1:
+    输入: [1,3,2,2,5,2,3,7]
+    输出: 5
+    原因: 最长的和谐数组是：[3,2,2,2,3].
+    说明: 输入的数组长度最大不超过20,000.
+    :param nums: List[int]
+    :return: int
+    """
+    d = {}
+    l = len(nums)
+    m = 0
+    for i in range(l):
+        if nums[i] not in d:
+            d[nums[i]] = 0
+        d[nums[i]] += 1
+    for n in nums:
+        if n - 1 in d:
+            m = max(m, d[n] + d[n - 1])
+    return m
 
 if __name__ == '__main__':
     pass
-    print(longestWord(["a","banana","app","appl","ap","apply","apple"]))
+    # print(longestWord(["a","banana","app","appl","ap","apply","apple"]))
+    print(findLHS([1,2,2,1]))
     # print(bitwiseComplement(5))
     # print(hammingWeight(0b00000000000000000000000000001011))
     # print(reverseBits(0b00000010100101000001111010011100))

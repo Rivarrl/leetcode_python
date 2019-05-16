e227468c2c57bc81484f4f560089069ba4af3358
rivarrl <1049793871@qq.com>
2019-05-17 00:20:45 +0800

    add

--- leetcode4.py
+++ leetcode4.py
@@ -87,7 +87,45 @@ def searchRange(nums, target):
     return [i, j]
 
 
+def reverseKGroup(head, k):
+    """
+    25. k个一组翻转链表
+    给出一个链表，每 k 个节点一组进行翻转，并返回翻转后的链表。
+    k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么将最后剩余节点保持原有顺序。
+    示例 :
+    给定这个链表：1->2->3->4->5
+    当 k = 2 时，应当返回: 2->1->4->3->5
+    当 k = 3 时，应当返回: 3->2->1->4->5
+    说明 :
+    你的算法只能使用常数的额外空间。
+    你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+    :param head: ListNode
+    :param k: int
+    :return: ListNode
+    """
+    p, q, cnt = head, head, 0
+    while q.next and q.next.next:
+        p = p.next
+        q = q.next.next
+        cnt += 1
+    l = cnt * 2 if q.next else cnt * 2 - 1
+    p, q = head, head
+    while l > k:
+        x = k - 1
+        while x > 0:
+            r = q
+            q = p.next
+            p.next = None if not p.next.next else p.next.next
+            q.next = r
+            x -= 1
+        l -= k
+    return q
+
+
 if __name__ == '__main__':
+    x = construct_list_node([1,2,3,4,5])
+    reverseKGroup(x,2)
+    print_list_node(x)
     # print(jump([2, 3, 1, 1, 4]))
-    searchRange([1,1,2], 1)
+    # searchRange([1,1,2], 1)
     pass

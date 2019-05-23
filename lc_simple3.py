753e8571155f0a8ccd07ee383f50df58086ee5c5
rivarrl <tianbao.liu@iquantex.com>
2019-05-23 18:01:39 +0800

    fix

--- lc_simple3.py
+++ lc_simple3.py
@@ -311,9 +311,47 @@ def shortestToChar(S, C):
     return res
 
 
+def findDisappearedNumbers(nums):
+    """
+    448. 找到所有数组中消失的数字
+    给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+    找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+    您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+    示例:
+    输入:
+    [4,3,2,7,8,2,3,1]
+    输出:
+    [5,6]
+    :param nums: List[int]
+    :return: List[int]
+    """
+    """
+    # 座位交换法
+    n = len(nums)
+    for i in range(n):
+        while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+            nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+    res = []
+    for i in range(n):
+        if nums[i] != i + 1:
+            res.append(i + 1)
+    return res
+    """
+    # 置负值法
+    n = len(nums)
+    for i in range(n):
+        nums[abs(nums[i]) - 1] = -abs(nums[abs(nums[i]) - 1])
+    res = []
+    for i in range(n):
+        if nums[i] != i + 1:
+            res.append(i + 1)
+    return res
+
+
 if __name__ == '__main__':
     pass
-    findNthDigit(10)
+    findDisappearedNumbers([4,3,2,7,8,2,3,1])
+    # findNthDigit(10)
     # x = construct_tree_node([1,2,3,None,5])
     # print(binaryTreePaths(x))
     # x = construct_list_node([1,2])

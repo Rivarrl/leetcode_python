ed4f2ee7165b91452dec8ebeee7c15fb34d76dc6
Rivarrl <1049793871@qq.com>
2019-05-24 23:43:45 +0800

    fix

--- lc_simple3.py
+++ lc_simple3.py
@@ -407,11 +407,46 @@ def pathSum(root, sum):
     helper(root, 0)
     return ans[0]
 
+
+def isUgly(num):
+    """
+    263. 丑数
+    编写一个程序判断给定的数是否为丑数。
+    丑数就是只包含质因数 2, 3, 5 的正整数。
+    示例 1:
+    输入: 6
+    输出: true
+    解释: 6 = 2 × 3
+    示例 2:
+    输入: 8
+    输出: true
+    解释: 8 = 2 × 2 × 2
+    示例 3:
+    输入: 14
+    输出: false
+    解释: 14 不是丑数，因为它包含了另外一个质因数 7。
+    说明：
+    1 是丑数。
+    输入不会超过 32 位有符号整数的范围: [−231,  231 − 1]。
+    :param num: int
+    :return: bool
+    """
+    if num <= 0: return False
+    while num % 2 == 0:
+        num//=2
+    while num % 3 == 0:
+        num//=3
+    while num % 5 == 0:
+        num//=5
+    return num == 1
+
+
 if __name__ == '__main__':
     pass
-    x = construct_tree_node([10,5,-3,3,2,None,11,3,-2,None,1])
-    ans = pathSum(x, 8)
-    print(ans)
+    print(isUgly(14))
+    # x = construct_tree_node([10,5,-3,3,2,None,11,3,-2,None,1])
+    # ans = pathSum(x, 8)
+    # print(ans)
     # findDisappearedNumbers([4,3,2,7,8,2,3,1])
     # findNthDigit(10)
     # x = construct_tree_node([1,2,3,None,5])

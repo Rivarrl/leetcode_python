4cdb4e842d96652f7caf9b3670c051c7fb8fee99
rivarrl <1049793871@qq.com>
2019-06-06 00:48:55 +0800

    add

--- lc_simple3.py
+++ lc_simple3.py
@@ -650,8 +650,40 @@ def gcdOfStrings(str1, str2):
     return str1
 
 
+def merge(nums1, m, nums2, n):
+    """
+    88. 合并两个有序数组
+    给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+    说明:
+    初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+    你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+    示例:
+    输入:
+    nums1 = [1,2,3,0,0,0], m = 3
+    nums2 = [2,5,6],       n = 3
+    输出: [1,2,2,3,5,6]
+    :param nums1: List[int]
+    :param m: int
+    :param nums2: List[int]
+    :param n: int
+    :return: None
+    """
+    if m == 0:
+        for i in range(n):
+            nums1[i] = nums2[i]
+    while m + n > 0:
+        if nums1[m-1] > nums2[n-1]:
+            nums1[m+n-1] = nums1[m-1]
+            m -= 1
+        else:
+            nums1[m+n-1] = nums2[n-1]
+            n -= 1
+    print(nums1)
+
+
 if __name__ == '__main__':
-    print(gcdOfStrings("AAAAAAAA", "AAAAAAAA"))
+    merge([2,0], 1, [1], 1)
+    # print(gcdOfStrings("AAAAAAAA", "AAAAAAAA"))
     # findContentChildren([1,2], [1,2,3])
     # print(canPlaceFlowers([0], 1))
     # print(isPowerOfFour(16))
--- leetcode.py
+++ leetcode.py
@@ -842,19 +842,6 @@ class Solution:
             i += 1
         return res
 
-    def merge(self, nums1, m, nums2, n):
-        """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: void Do not return anything, modify nums1 in-place instead.
-        """
-        while m < len(nums1):
-            nums1.pop(m)
-        nums1.extend(nums2)
-        nums1.sort()
-
     def reverseWords(self, s):
         """
         :type s: str

e10096ab69c6ada23eb9555ac9064f0a6eff8ce8
Rivarrl <1049793871@qq.com>
2019-06-09 01:32:17 +0800

    new: 73. 矩阵置零

--- leetcode5.py
+++ leetcode5.py
@@ -134,8 +134,66 @@ def levelOrder(root):
     return res
 
 
+def setZeroes(matrix):
+    """
+    73. 矩阵置零
+    给定一个 m x n 的矩阵，如果一个元素为 0，则将其所在行和列的所有元素都设为 0。请使用原地算法。
+    示例 1:
+    输入:
+    [
+      [1,1,1],
+      [1,0,1],
+      [1,1,1]
+    ]
+    输出:
+    [
+      [1,0,1],
+      [0,0,0],
+      [1,0,1]
+    ]
+    示例 2:
+    输入:
+    [
+      [0,1,2,0],
+      [3,4,5,2],
+      [1,3,1,5]
+    ]
+    输出:
+    [
+      [0,0,0,0],
+      [0,4,5,0],
+      [0,3,1,0]
+    ]
+    进阶:
+    一个直接的解决方案是使用  O(mn) 的额外空间，但这并不是一个好的解决方案。
+    一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。
+    你能想出一个常数空间的解决方案吗？
+    :param matrix: List[List[int]]
+    :return: None
+    """
+    n = len(matrix)
+    m = len(matrix[0])
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == '0':
+                continue
+            if matrix[i][j] == 0:
+                for k in range(m):
+                    matrix[i][k] = '0' if matrix[i][k] != 0 else 0
+                for q in range(n):
+                    matrix[q][j] = '0' if matrix[q][j] != 0 else 0
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == '0':
+                matrix[i][j] = 0
+
+
 if __name__ == '__main__':
-    x = construct_tree_node([3,9,20,None,None,15,7])
-    levelOrder(x)
+    x = [[0,1,2,0],
+         [3,4,5,2],
+         [1,3,1,5]]
+    setZeroes(x)
+    # x = construct_tree_node([3,9,20,None,None,15,7])
+    # levelOrder(x)
     # print(canCompleteCircuit([3,3,4],[3,4,4]))
     pass

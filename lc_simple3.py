d6633984232da5be48ff5b0a06ce100c9b11d9a4
Rivarrl <1049793871@qq.com>
2019-06-10 00:22:41 +0800

    new: 349. 两个数组的交集

--- lc_simple3.py
+++ lc_simple3.py
@@ -730,6 +730,41 @@ def missingNumber(nums):
     return ans
 
 
+def intersection(nums1, nums2):
+    """
+    349. 两个数组的交集
+    给定两个数组，编写一个函数来计算它们的交集。
+    示例 1:
+    输入: nums1 = [1,2,2,1], nums2 = [2,2]
+    输出: [2]
+    示例 2:
+    输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+    输出: [9,4]
+    说明:
+    输出结果中的每个元素一定是唯一的。
+    我们可以不考虑输出结果的顺序。
+    :param nums1: List[int]
+    :param nums2: List[int]
+    :return: List[int]
+    """
+    nums1 = sorted(list(set(nums1)))
+    nums2 = sorted(list(set(nums2)))
+    n1, n2 = len(nums1), len(nums2)
+    i, j = 0, 0
+    res = []
+    while i < n1 and j < n2:
+        if nums1[i] == nums2[j]:
+            res.append(nums1[i])
+            i += 1
+            j += 1
+        else:
+            if nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+    return res
+
+
 if __name__ == '__main__':
     print(missingNumber([3, 0, 1]))
     # merge([2,0], 1, [1], 1)

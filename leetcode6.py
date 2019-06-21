e0e9f5b33258209c0793d4c91e7140aa21027efb
rivarrl <tianbao.liu@iquantex.com>
2019-06-21 18:06:37 +0800

    fix

---
+++ leetcode6.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+from algorithm_utils import *
+
+# leetcode 6
+
+def partition(head, x):
+    """
+    86. 分隔链表
+    给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+    你应当保留两个分区中每个节点的初始相对位置。
+    示例:
+    输入: head = 1->4->3->2->5->2, x = 3
+    输出: 1->2->2->4->3->5
+    :param head: ListNode
+    :param x: int
+    :return: ListNode
+    """
+    pass
+
+
+def kthSmallest(matrix, k):
+    """
+    378. 有序矩阵中第K小的元素
+    给定一个 n x n 矩阵，其中每行和每列元素均按升序排序，找到矩阵中第k小的元素。
+    请注意，它是排序后的第k小元素，而不是第k个元素。
+    示例:
+    matrix = [
+       [ 1,  5,  9],
+       [10, 11, 13],
+       [12, 13, 15]
+    ],
+    k = 8,
+    返回 13。
+    说明:
+    你可以假设 k 的值永远是有效的, 1 ≤ k ≤ n2 。
+    :param matrix: List[List[int]]
+    :param k: int
+    :return: int
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass

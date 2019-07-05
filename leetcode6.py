645f2ecadb8183d5865278bd81721c79b030c61f
rivarrl <tianbao.liu@iquantex.com>
2019-07-05 16:36:02 +0800

    fix

--- leetcode6.py
+++ leetcode6.py
@@ -579,7 +579,8 @@ def containsNearbyAlmostDuplicate(nums, k, t):
     :param t: int
     :return: bool
     """
-    # 超时，用BST
+    """
+    # 普通做法，超时
     n = len(nums)
     if n < 2: return False
     for j in range(1, k+1):
@@ -587,34 +588,18 @@ def containsNearbyAlmostDuplicate(nums, k, t):
             if abs(nums[i] - nums[i+j]) <= t:
                 return True
     return False
-
-
-def findRotateSteps(ring, key):
     """
-    514. 自由之路
-    视频游戏“辐射4”中，任务“通向自由”要求玩家到达名为“Freedom Trail Ring”的金属表盘，并使用表盘拼写特定关键词才能开门。
-    给定一个字符串 ring，表示刻在外环上的编码；给定另一个字符串 key，表示需要拼写的关键词。您需要算出能够拼写关键词中所有字符的最少步数。
-    最初，ring 的第一个字符与12:00方向对齐。您需要顺时针或逆时针旋转 ring 以使 key 的一个字符在 12:00 方向对齐，然后按下中心按钮，以此逐个拼写完 key 中的所有字符。
-    旋转 ring 拼出 key 字符 key[i] 的阶段中：
-    您可以将 ring 顺时针或逆时针旋转一个位置，计为1步。旋转的最终目的是将字符串 ring 的一个字符与 12:00 方向对齐，并且这个字符必须等于字符 key[i] 。
-    如果字符 key[i] 已经对齐到12:00方向，您需要按下中心按钮进行拼写，这也将算作 1 步。按完之后，您可以开始拼写 key 的下一个字符（下一阶段）, 直至完成所有拼写。
-    示例：
-    输入: ring = "godding", key = "gd"
-    输出: 4
-    解释:
-     对于 key 的第一个字符 'g'，已经在正确的位置, 我们只需要1步来拼写这个字符。
-     对于 key 的第二个字符 'd'，我们需要逆时针旋转 ring "godding" 2步使它变成 "ddinggo"。
-     当然, 我们还需要1步进行拼写。
-     因此最终的输出是 4。
-    提示：
-    ring 和 key 的字符串长度取值范围均为 1 至 100；
-    两个字符串中都只有小写字符，并且均可能存在重复字符；
-    字符串 key 一定可以由字符串 ring 旋转拼出。
-    :param ring: str
-    :param key: str
-    :return: int
-    """
-    pass
+    # 记录位置并排序 20%
+    n = len(nums)
+    if n < 2: return False
+    arr = sorted([[x, i] for i, x in enumerate(nums)])
+    for i in range(n):
+        for j in range(i+1, n):
+            if arr[j][0] - arr[i][0] > t:
+                break
+            if abs(arr[i][1] - arr[j][1]) <= k:
+                return True
+    return False
 
 
 def maxSlidingWindow(nums, k):
@@ -814,6 +799,34 @@ def longestIncreasingPath(matrix):
                 dp[i][j] = max(dp[x][y] + 1, dp[i][j])
                 ans = max(dp[i][j], ans)
     return ans
+
+
+def findRotateSteps(ring, key):
+    """
+    514. 自由之路
+    视频游戏“辐射4”中，任务“通向自由”要求玩家到达名为“Freedom Trail Ring”的金属表盘，并使用表盘拼写特定关键词才能开门。
+    给定一个字符串 ring，表示刻在外环上的编码；给定另一个字符串 key，表示需要拼写的关键词。您需要算出能够拼写关键词中所有字符的最少步数。
+    最初，ring 的第一个字符与12:00方向对齐。您需要顺时针或逆时针旋转 ring 以使 key 的一个字符在 12:00 方向对齐，然后按下中心按钮，以此逐个拼写完 key 中的所有字符。
+    旋转 ring 拼出 key 字符 key[i] 的阶段中：
+    您可以将 ring 顺时针或逆时针旋转一个位置，计为1步。旋转的最终目的是将字符串 ring 的一个字符与 12:00 方向对齐，并且这个字符必须等于字符 key[i] 。
+    如果字符 key[i] 已经对齐到12:00方向，您需要按下中心按钮进行拼写，这也将算作 1 步。按完之后，您可以开始拼写 key 的下一个字符（下一阶段）, 直至完成所有拼写。
+    示例：
+    输入: ring = "godding", key = "gd"
+    输出: 4
+    解释:
+     对于 key 的第一个字符 'g'，已经在正确的位置, 我们只需要1步来拼写这个字符。
+     对于 key 的第二个字符 'd'，我们需要逆时针旋转 ring "godding" 2步使它变成 "ddinggo"。
+     当然, 我们还需要1步进行拼写。
+     因此最终的输出是 4。
+    提示：
+    ring 和 key 的字符串长度取值范围均为 1 至 100；
+    两个字符串中都只有小写字符，并且均可能存在重复字符；
+    字符串 key 一定可以由字符串 ring 旋转拼出。
+    :param ring: str
+    :param key: str
+    :return: int
+    """
+    pass
 
 
 if __name__ == '__main__':

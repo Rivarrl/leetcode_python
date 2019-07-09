f7ae169ca2b5ae746476cfa1588bdfbe208a3ac6
rivarrl <1049793871@qq.com>
2019-07-09 22:41:30 +0800

    add

--- simple4.py
+++ simple4.py
@@ -547,6 +547,41 @@ def findRadius(houses, heaters):
     return ans
 
 
+def thirdMax(nums):
+    """
+    414. 第三大的数
+    给定一个非空数组，返回此数组中第三大的数。如果不存在，则返回数组中最大的数。要求算法时间复杂度必须是O(n)。
+    示例 1:
+    输入: [3, 2, 1]
+    输出: 1
+    解释: 第三大的数是 1.
+    示例 2:
+    输入: [1, 2]
+    输出: 2
+    解释: 第三大的数不存在, 所以返回最大的数 2 .
+    示例 3:
+    输入: [2, 2, 3, 1]
+    输出: 1
+    解释: 注意，要求返回第三大的数，是指第三大且唯一出现的数。
+    存在两个值为2的数，它们都排第二。
+    :param nums: List[int]
+    :return: int
+    """
+    max1, max2, max3 = float('-inf'), float('-inf'), float('-inf')
+    for num in nums:
+        if num > max1:
+            max3, max2 = max2, max1
+            max1 = num
+        elif max2 < num < max1:
+            max3 = max2
+            max2 = num
+        elif max3 < num < max2:
+            max3 = num
+    return max1 if max3 == float('-inf') else max3
+
+
+
+
 if __name__ == '__main__':
     findRadius([1,5], [10])
     # findRelativeRanks([10,3,8,9,4])

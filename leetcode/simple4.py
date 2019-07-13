58aaf8d7f99dd4dd070bcc79e5f55021f06536f9
rivarrl <1049793871@qq.com>
2019-07-13 23:52:15 +0800

    add

--- leetcode/simple4.py
+++ leetcode/simple4.py
@@ -579,10 +579,36 @@ def thirdMax(nums):
     return max1 if max3 == float('-inf') else max3
 
 
+def transpose(A):
+    """
+    867. 转置矩阵
+    给定一个矩阵 A， 返回 A 的转置矩阵。
+    矩阵的转置是指将矩阵的主对角线翻转，交换矩阵的行索引与列索引。
+    示例 1：
+    输入：[[1,2,3],[4,5,6],[7,8,9]]
+    输出：[[1,4,7],[2,5,8],[3,6,9]]
+    示例 2：
+    输入：[[1,2,3],[4,5,6]]
+    输出：[[1,4],[2,5],[3,6]]
+    提示：
+    1 <= A.length <= 1000
+    1 <= A[0].length <= 1000
+    :param A: List[int]
+    :return: List[int]
+    """
+    n = len(A)
+    if n == 0: return A
+    m = len(A[0])
+    B = [[0 for _ in range(n)] for _ in range(m)]
+    for i in range(m):
+        for j in range(n):
+            B[i][j] = A[j][i]
+    return B
 
 
 if __name__ == '__main__':
-    findRadius([1,5], [10])
+    transpose([[1,2,3],[4,5,6]])
+    # findRadius([1,5], [10])
     # findRelativeRanks([10,3,8,9,4])
     # checkPerfectNumber(28)
     # numMovesStones(1,5,2)

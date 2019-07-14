d5d39d4ae01fbe78fcaac8af98b96fbc6d869ad3
rivarrl <1049793871@qq.com>
2019-07-14 23:00:17 +0800

    add

--- leetcode/simple4.py
+++ leetcode/simple4.py
@@ -606,8 +606,55 @@ def transpose(A):
     return B
 
 
+def surfaceArea(grid):
+    """
+    892. 三维形体的表面积
+    在 N * N 的网格上，我们放置一些 1 * 1 * 1  的立方体。
+    每个值 v = grid[i][j] 表示 v 个正方体叠放在对应单元格 (i, j) 上。
+    请你返回最终形体的表面积。
+    示例 1：
+    输入：[[2]]
+    输出：10
+    示例 2：
+    输入：[[1,2],[3,4]]
+    输出：34
+    示例 3：
+    输入：[[1,0],[0,2]]
+    输出：16
+    示例 4：
+    输入：[[1,1,1],[1,0,1],[1,1,1]]
+    输出：32
+    示例 5：
+    输入：[[2,2,2],[2,1,2],[2,2,2]]
+    输出：46
+    提示：
+    1 <= N <= 50
+    0 <= grid[i][j] <= 50
+    :param grid: List[List[int]]
+    :return: int
+    """
+    n = len(grid)
+    if n == 0: return 0
+    m = len(grid[0])
+    each_sum = lambda x: 0 if x == 0 else 4 * x + 2
+    es, cs = 0, 0
+    for i in range(n):
+        for j in range(m):
+            es += each_sum(grid[i][j])
+            down = 0 if i == n - 1 else min(grid[i][j], grid[i+1][j])
+            right = 0 if j == m - 1 else min(grid[i][j], grid[i][j+1])
+            cs += right + down
+    ans = es - cs * 2
+    return ans
+
+
 if __name__ == '__main__':
-    transpose([[1,2,3],[4,5,6]])
+    surfaceArea([[2]])
+    surfaceArea([[1,2],[3,4]])
+    surfaceArea([[1,0],[0,2]])
+    surfaceArea([[1,1,1],[1,0,1],[1,1,1]])
+    surfaceArea([[2,2,2],[2,1,2],[2,2,2]])
+    # transpose([[1,2,3],[4,5,6]])
     # findRadius([1,5], [10])
     # findRelativeRanks([10,3,8,9,4])
     # checkPerfectNumber(28)

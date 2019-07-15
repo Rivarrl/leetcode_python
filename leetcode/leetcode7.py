64bbf9518d749512136ef79568608895718650c6
rivarrl <tianbao.liu@iquantex.com>
2019-07-15 18:30:54 +0800

    fix

--- leetcode/leetcode7.py
+++ leetcode/leetcode7.py
@@ -205,6 +205,53 @@ def recoverFromPreorder(S):
     return res[0][0]
 
 
+def buildTree(preorder, inorder):
+    """
+    105. 从前序与中序遍历序列构造二叉树
+    根据一棵树的前序遍历与中序遍历构造二叉树。
+    注意:
+    你可以假设树中没有重复的元素。
+    例如，给出
+    前序遍历 preorder = [3,9,20,15,7]
+    中序遍历 inorder = [9,3,15,20,7]
+    返回如下的二叉树：
+        3
+       / \
+      9  20
+        /  \
+       15   7
+    :param preorder: List[int]
+    :param inorder: List[int]
+    :return: TreeNode
+    """
+    pass
+
+
+
+
+def buildTree2(inorder, postorder):
+    """
+    106. 从中序与后序遍历序列构造二叉树
+    根据一棵树的中序遍历与后序遍历构造二叉树。
+    注意:
+    你可以假设树中没有重复的元素。
+    例如，给出
+    中序遍历 inorder = [9,3,15,20,7]
+    后序遍历 postorder = [9,15,7,20,3]
+    返回如下的二叉树：
+        3
+       / \
+      9  20
+        /  \
+       15   7
+    :param inorder: List[int]
+    :param postorder: List[int]
+    :return: TreeNode
+    """
+    pass
+
+
+
 if __name__ == '__main__':
     # x = construct_tree_node([1,2,3,4,5,6])
     # countNodes(x)

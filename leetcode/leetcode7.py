f6844f10526b25ea67740a305bf0f5c6e1888e4b
rivarrl <tianbao.liu@iquantex.com>
2019-07-25 18:52:52 +0800

    fix

--- leetcode/leetcode7.py
+++ leetcode/leetcode7.py
@@ -691,9 +691,78 @@ def largestValues(root):
     return res
 
 
+def findAndReplacePattern(words, pattern):
+    """
+    890. 查找和替换模式
+    你有一个单词列表 words 和一个模式  pattern，你想知道 words 中的哪些单词与模式匹配。
+    如果存在字母的排列 p ，使得将模式中的每个字母 x 替换为 p(x) 之后，我们就得到了所需的单词，那么单词与模式是匹配的。
+    （回想一下，字母的排列是从字母到字母的双射：每个字母映射到另一个字母，没有两个字母映射到同一个字母。）
+    返回 words 中与给定模式匹配的单词列表。
+    你可以按任何顺序返回答案。
+    示例：
+    输入：words = ["abc","deq","mee","aqq","dkd","ccc"], pattern = "abb"
+    输出：["mee","aqq"]
+    解释：
+    "mee" 与模式匹配，因为存在排列 {a -> m, b -> e, ...}。
+    "ccc" 与模式不匹配，因为 {a -> c, b -> c, ...} 不是排列。
+    因为 a 和 b 映射到同一个字母。
+    提示：
+    1 <= words.length <= 50
+    1 <= pattern.length = words[i].length <= 20
+    :param words: List[str]
+    :param pattern: str
+    :return: List[str]
+    """
+    """
+    # 字典
+    def helper(s):
+        dp = {}
+        for i in range(len(s)):
+            if s[i] not in dp:
+                dp[s[i]] = i
+            if ds[pattern[i]] != dp[s[i]]: return False
+        return True
+
+    res = []
+    ds = {}
+    for i in range(len(pattern)):
+        if pattern[i] not in ds:
+            ds[pattern[i]] = i
+
+    for each in words:
+        if helper(each):
+            res.append(each)
+    return res
+    """
+    # 数组
+    def helper(word):
+        dp = [-1] * 26
+        for i in range(len(word)):
+            p = ord(pattern[i]) - a
+            s = ord(word[i]) - a
+            if dp[s] < 0:
+                dp[s] = i
+            if ds[p] != dp[s]: return False
+        return True
+
+    a = ord('a')
+    ds = [-1] * 26
+    for i in range(len(pattern)):
+        p = ord(pattern[i]) - a
+        if ds[p] < 0:
+            ds[p] = i
+
+    res = []
+    for each in words:
+        if helper(each):
+            res.append(each)
+    return res
+
+
 if __name__ == '__main__':
-    x = construct_tree_node([1,3,2,5,3,null,9])
-    largestValues(x)
+    findAndReplacePattern(words = ["abc","deq","mee","aqq","dkd","ccc"], pattern = "abb")
+    # x = construct_tree_node([1,3,2,5,3,null,9])
+    # largestValues(x)
     # minMutation("AAAAACCC","AACCCCCC",["AAAACCCC", "AAACCCCC", "AACCCCCC"])
     # poorPigs(4, 15, 15)
     # print(checkRecord(13))

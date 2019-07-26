a9a78beb367964eea47cbf2dbc4534df6c736864
rivarrl <tianbao.liu@iquantex.com>
2019-07-26 18:34:08 +0800

    fix

--- leetcode/leetcode7.py
+++ leetcode/leetcode7.py
@@ -836,6 +836,26 @@ def isNStraightHand(hand, W):
     return all(x == r[0] for x in r)
 
 
+def constructFromPrePost(pre, post):
+    """
+    889. 根据前序和后序遍历构造二叉树
+    返回与给定的前序和后序遍历匹配的任何二叉树。
+    pre 和 post 遍历中的值是不同的正整数。
+    示例：
+    输入：pre = [1,2,4,5,3,6,7], post = [4,5,2,6,7,3,1]
+    输出：[1,2,3,4,5,6,7]
+    提示：
+    1 <= pre.length == post.length <= 30
+    pre[] 和 post[] 都是 1, 2, ..., pre.length 的排列
+    每个输入保证至少有一个答案。如果有多个答案，可以返回其中一个。
+    :param pre: List[int]
+    :param post: List[int]
+    :return: TreeNode
+    """
+
+
+
+
 if __name__ == '__main__':
     ans = isNStraightHand(hand = [1,2,3,6,2,3,4,7,8], W = 3)
     # ans = isNStraightHand(hand = [1,1,2,2,3,3], W = 3)

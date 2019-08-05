6fa9dfbddef46fc9df87190aa643a66ac7795270
rivarrl <tianbao.liu@iquantex.com>
2019-08-05 18:29:33 +0800

    fix

--- leetcode/simple5.py
+++ leetcode/simple5.py
@@ -191,6 +191,29 @@ def rotatedDigits(N):
     return ans
 
 
+def maxDepth(root):
+    """
+    559. N叉树的最大深度
+    给定一个 N 叉树，找到其最大深度。
+    最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。
+    例如，给定一个 3叉树 :
+            1
+        3   2   4
+      5   6
+    我们应返回其最大深度，3。
+    说明:
+    树的深度不会超过 1000。
+    树的节点总不会超过 5000。
+    :param root: Node
+    :return: int
+    """
+    if not root: return 0
+    cur = 0
+    for x in root.children:
+        cur = max(maxDepth(x), cur)
+    return cur + 1
+
+
 if __name__ == '__main__':
     rotatedDigits(9)
     # lastStoneWeight([2,7,4,1,8,1])

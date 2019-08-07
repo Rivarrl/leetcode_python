a09d8c957b4c8ed27a0d7365a678fe64369f70dd
rivarrl <tianbao.liu@iquantex.com>
2019-08-07 18:26:03 +0800

    fix

--- leetcode/leetcode_hr.py
+++ leetcode/leetcode_hr.py
@@ -49,7 +49,66 @@ def removeInvalidParentheses(s):
     pass
 
 
+def crackSafe(n, k):
+    """
+    753. 破解保险箱
+    有一个需要密码才能打开的保险箱。密码是 n 位数, 密码的每一位是 k 位序列 0, 1, ..., k-1 中的一个 。
+    你可以随意输入密码，保险箱会自动记住最后 n 位输入，如果匹配，则能够打开保险箱。
+    举个例子，假设密码是 "345"，你可以输入 "012345" 来打开它，只是你输入了 6 个字符.
+    请返回一个能打开保险箱的最短字符串。
+    示例1:
+    输入: n = 1, k = 2
+    输出: "01"
+    说明: "10"也可以打开保险箱。
+    示例2:
+    输入: n = 2, k = 2
+    输出: "00110"
+    说明: "01100", "10011", "11001" 也能打开保险箱。
+    提示：
+    n 的范围是 [1, 4]。
+    k 的范围是 [1, 10]。
+    k^n 最大可能为 4096。
+    :param n: int
+    :param k: int
+    :return: str
+    """
+    """
+    # set 67%
+    res = '0' * n
+    s = set()
+    s.add(res)
+    for i in range(k ** n + 1):
+        pre = res[len(res) - n + 1:]
+        for j in range(k-1, -1, -1):
+            cur = pre + str(j)
+            if not cur in s:
+                s.add(cur)
+                res += str(j)
+                break
+    return res
+    """
+    # dfs
+    def dfs():
+        if (len(s) == total): return True
+        nonlocal res
+        pre = "".join(res[len(res) - n + 1:])
+        for j in range(k):
+            cur = pre + str(j)
+            if cur not in s:
+                res.append(str(j))
+                s.add(cur)
+                if dfs(): return True
+                res.pop()
+                s.remove(cur)
+    res = ["0"] * n
+    total = k ** n
+    s = set()
+    s.add("".join(res))
+    return "".join(res) if dfs() else ""
+
 
 if __name__ == '__main__':
-    removeBoxes([1, 3, 2, 2, 2, 3, 4, 3, 1])
+    y = crackSafe(2, 3)
+    print(y)
+    # removeBoxes([1, 3, 2, 2, 2, 3, 4, 3, 1])
     pass

a098b550427d6f1bfd53e2373d64a47eef04c9c5
rivarrl <1049793871@qq.com>
2019-08-08 23:36:10 +0800

    add

--- leetcode/leetcode_nm.py
+++ leetcode/leetcode_nm.py
@@ -265,6 +265,35 @@ def maxProduct(words):
     return max([d[x] * d[y] for x in d for y in d if not x & y] or [0])
 
 
+def oddEvenList(head):
+    """
+    328. 奇偶链表
+    给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+    请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+    示例 1:
+    输入: 1->2->3->4->5->NULL
+    输出: 1->3->5->2->4->NULL
+    示例 2:
+    输入: 2->1->3->5->6->4->7->NULL
+    输出: 2->3->6->7->1->5->4->NULL
+    说明:
+    应当保持奇数节点和偶数节点的相对顺序。
+    链表的第一个节点视为奇数节点，第二个节点视为偶数节点，以此类推。
+    :param head: ListNode
+    :return: ListNode
+    """
+    if not head or not head.next:
+        return head
+    p1, p2, p3 = head, head.next, head.next
+    while p1.next and p2.next:
+        p1.next = p2.next
+        p1 = p1.next
+        p2.next = p1.next
+        p2 = p2.next
+    p1.next = p3
+    return head
+
+
 def lastStoneWeightII(stones):
     """
     1049. 最后一块石头的重量 II
@@ -290,7 +319,36 @@ def lastStoneWeightII(stones):
     pass
 
 
+def findDuplicates(nums):
+    """
+    442. 数组中重复的数据
+    给定一个整数数组 a，其中1 ≤ a[i] ≤ n （n为数组长度）, 其中有些元素出现两次而其他元素出现一次。
+    找到所有出现两次的元素。
+    你可以不用到任何额外空间并在O(n)时间复杂度内解决这个问题吗？
+    示例：
+    输入:
+    [4,3,2,7,8,2,3,1]
+    输出:
+    [2,3]
+    :param nums: List[int]
+    :return: List[int]
+    """
+    # 交换与下标不对应的数字，遇到nums[i] == nums[nums[i]]时则重复
+    res = set()
+    for i, x in enumerate(nums):
+        while nums[i] != i + 1:
+            if nums[i] == nums[nums[i] - 1]:
+                res.add(nums[i])
+                break
+            j = nums[i] - 1
+            nums[i], nums[j] = nums[j], nums[i]
+    return list(res)
+
+
 if __name__ == '__main__':
+    findDuplicates([4,3,2,7,8,2,3,1])
+    # x = construct_list_node([1,2,3,4,5])
+    # oddEvenList(x)
     # maxProduct(["eae","ea","aaf","bda","fcf","dc","ac","ce","cefde","dabae"])
     # x = construct_list_node([-10,-3,0,5,9])
     # y = sortedListToBST(x)

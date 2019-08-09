3853f52a4246e2c8fdecae70fad6c26a5a635da0
Rivarrl <1049793871@qq.com>
2019-08-10 00:27:14 +0800

    add

--- leetcode/simple5.py
+++ leetcode/simple5.py
@@ -214,8 +214,41 @@ def maxDepth(root):
     return cur + 1
 
 
+def largestTriangleArea(points):
+    """
+    812. 最大三角形面积
+    给定包含多个点的集合，从其中取三个点组成三角形，返回能组成的最大三角形的面积。
+    示例:
+    输入: points = [[0,0],[0,1],[1,0],[0,2],[2,0]]
+    输出: 2
+    解释:
+    这五个点如下图所示。组成的橙色三角形是最大的，面积为2。
+    注意:
+    3 <= points.length <= 50.
+    不存在重复的点。
+     -50 <= points[i][j] <= 50.
+    结果误差值在 10^-6 以内都认为是正确答案。
+    :param points: List[List[int]]
+    :return: float
+    """
+    # 三层暴力 + 向量求平行四边形面积/2 or 海伦公式 p = (a+b+c)/2  area=sqrt(p*(p-a)*(p-b)*(p-c))
+    area = 0
+    for i in range(len(points) - 2):
+        for k in range(i + 1, len(points) - 1):
+            for m in range(k + 1, len(points)):
+                x1 = points[i][0]
+                y1 = points[i][1]
+                x2 = points[k][0]
+                y2 = points[k][1]
+                x3 = points[m][0]
+                y3 = points[m][1]
+                area = max(area, abs((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3)))
+    return (area / 2)
+
+
 if __name__ == '__main__':
-    rotatedDigits(9)
+    largestTriangleArea([[0,0],[0,1],[1,0],[0,2],[2,0]])
+    # rotatedDigits(9)
     # lastStoneWeight([2,7,4,1,8,1])
     # distributeCandies(10, 3)
     # tribonacci(25)

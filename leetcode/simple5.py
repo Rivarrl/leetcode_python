7022d8d0a6b70113ee40d71d78bcf95c6d53fd56
Rivarrl <1049793871@qq.com>
2019-08-11 21:57:48 +0800

    add

--- leetcode/simple5.py
+++ leetcode/simple5.py
@@ -246,6 +246,30 @@ def largestTriangleArea(points):
     return (area / 2)
 
 
+def minMoves(nums):
+    """
+    453. 最小移动次数使数组元素相等
+    给定一个长度为 n 的非空整数数组，找到让数组所有元素相等的最小移动次数。每次移动可以使 n - 1 个元素增加 1。
+    示例:
+    输入:
+    [1,2,3]
+    输出:
+    3
+    解释:
+    只需要3次移动（注意每次移动会增加两个元素的值）：
+    [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+    :param nums: List[int]
+    :return: int
+    """
+    # n-1加1分等价于1减1分
+    m = min(nums)
+    ans = 0
+    for x in nums:
+        ans += x - m
+    return ans
+
+
+
 if __name__ == '__main__':
     largestTriangleArea([[0,0],[0,1],[1,0],[0,2],[2,0]])
     # rotatedDigits(9)

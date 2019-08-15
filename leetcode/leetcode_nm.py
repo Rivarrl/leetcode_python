ce00c5e0ce9f4971b979181f3b6e2c1868ad5114
rivarrl <tianbao.liu@iquantex.com>
2019-08-15 18:47:02 +0800

    fix

--- leetcode/leetcode_nm.py
+++ leetcode/leetcode_nm.py
@@ -774,9 +774,102 @@ def findSubsequences(nums):
     return [list(x) for x in recursion(nums) if len(x) > 1]
 
 
+def distributeCoins(root):
+    """
+    979. 在二叉树中分配硬币
+    给定一个有 N 个结点的二叉树的根结点 root，树中的每个结点上都对应有 node.val 枚硬币，并且总共有 N 枚硬币。
+    在一次移动中，我们可以选择两个相邻的结点，然后将一枚硬币从其中一个结点移动到另一个结点。(移动可以是从父结点到子结点，或者从子结点移动到父结点。)。
+    返回使每个结点上只有一枚硬币所需的移动次数。
+    示例 1：
+    输入：[3,0,0]
+    输出：2
+    解释：从树的根结点开始，我们将一枚硬币移到它的左子结点上，一枚硬币移到它的右子结点上。
+    示例 2：
+    输入：[0,3,0]
+    输出：3
+    解释：从根结点的左子结点开始，我们将两枚硬币移到根结点上 [移动两次]。然后，我们把一枚硬币从根结点移到右子结点上。
+    示例 3：
+    输入：[1,0,2]
+    输出：2
+    示例 4：
+    输入：[1,0,0,null,3]
+    输出：4
+    提示：
+    1<= N <= 100
+    0 <= node.val <= N
+    :param root: TreeNode
+    :return: int
+    """
+    """
+    # abs
+    def dfs(p):
+        if not p: return 0
+        left, right = dfs(p.left), dfs(p.right)
+        nonlocal ans
+        ans += abs(left) + abs(right)
+        return p.val + left + right - 1
+    ans = 0
+    dfs(root)
+    return ans
+    """
+    # 节点数与求和对比
+    def dfs(p):
+        if not p: return 0, 0
+        left, right = dfs(p.left), dfs(p.right)
+        cv = p.val + left[0] + right[0]
+        cn = 1 + left[1] + right[1]
+        nonlocal ans
+        ans += abs(cn - cv)
+        return cv, cn
+    ans = 0
+    dfs(root)
+    return ans
+
+
+def pathInZigZagTree(label):
+    """
+    1104. 二叉树寻路
+    在一棵无限的二叉树上，每个节点都有两个子节点，树中的节点 逐行 依次按 “之” 字形进行标记。
+    如下图所示，在奇数行（即，第一行、第三行、第五行……）中，按从左到右的顺序进行标记；
+    而偶数行（即，第二行、第四行、第六行……）中，按从右到左的顺序进行标记。
+    给你树上某一个节点的标号 label，请你返回从根节点到该标号为 label 节点的路径，该路径是由途经的节点标号所组成的。
+                  1
+              3       2
+            4   5   6   7
+    示例 1：
+    输入：label = 14
+    输出：[1,3,4,14]
+    示例 2：
+    输入：label = 26
+    输出：[1,2,6,10,26]
+    提示：
+    1 <= label <= 10^6
+    :param label: int
+    :return: List[int]
+    """
+    x = 0
+    while label >> x:
+        x += 1
+    print(x)
+    res = []
+    while x > 1:
+        res.append(label)
+        if x % 2 == 0:
+            label = (label ^ ((1 << (x - 1)) - 1)) // 2
+        else:
+            label = ((1 << (x - 2))-1) ^ (label // 2)
+        x -= 1
+    res.append(1)
+    return res[::-1]
+
+
 if __name__ == '__main__':
-    a = findSubsequences([1,3,5,7])
-    print(a)
+    pathInZigZagTree(14)
+    # x = construct_tree_node([1,0,0,null,3])
+    # a = distributeCoins(x)
+    # print(a)
+    # a = findSubsequences([1,3,5,7])
+    # print(a)
     # findDiagonalOrder([[ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ]])
     # findTargetSumWays([1,1,1,1,1], 3)
     # x = construct_tree_node([5,2,-5])

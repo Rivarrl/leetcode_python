f56dba0ec90a4422528ff49688bc634cd9771ea2
Rivarrl <1049793871@qq.com>
2019-09-22 00:31:33 +0800

    add

--- leetcode/leetcode_nm2.py
+++ leetcode/leetcode_nm2.py
@@ -741,6 +741,28 @@ def findLength(A, B):
     return ans
 
 
+def rangeBitwiseAnd(m, n):
+    """
+    201. 数字范围按位与
+    给定范围 [m, n]，其中 0 <= m <= n <= 2147483647，返回此范围内所有数字的按位与（包含 m, n 两端点）。
+    示例 1: 
+    输入: [5,7]
+    输出: 4
+    示例 2:
+    输入: [0,1]
+    输出: 0
+    :param m: int
+    :param n: int
+    :return: int
+    """
+    i = 0
+    while m != n:
+        n >>= 1
+        m >>= 1
+        i += 1
+    return m << i
+
+
 if __name__ == '__main__':
     A = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
     B = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]

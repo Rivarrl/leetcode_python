95dc46786ed224710c830089f58fd85b525da196
Rivarrl <1049793871@qq.com>
2019-09-23 23:08:09 +0800

    add

--- leetcode/simple5.py
+++ leetcode/simple5.py
@@ -817,8 +817,40 @@ def isSubsequence(s, t):
     return False
 
 
+def maxNumberOfBalloons(text):
+    """
+    1189. "气球"的最大数量
+    给你一个字符串 text，你需要使用 text 中的字母来拼凑尽可能多的单词 "balloon"（气球）。
+    字符串 text 中的每个字母最多只能被使用一次。请你返回最多可以拼凑出多少个单词 "balloon"。
+    示例 1：
+    输入：text = "nlaebolko"
+    输出：1
+    示例 2：
+    输入：text = "loonbalxballpoon"
+    输出：2
+    示例 3：
+    输入：text = "leetcode"
+    输出：0
+    提示：
+    1 <= text.length <= 10^4
+    text 全部由小写英文字母组成
+    :param text: str
+    :return: int
+    """
+    j = 0
+    while j >= 0:
+        for x in "balloon":
+            print(text)
+            i = text.find(x)
+            if i >= 0:
+                text = text[:i] + text[i+1:]
+            else:
+                return j
+        j += 1
+
 
 if __name__ == '__main__':
+    maxNumberOfBalloons("nlaebolko")
     # distanceBetweenBusStops([0,11,6,1,4,3], 0, 5)
     # dayOfTheWeek(9, 9, 2019)
     # shortestCompletingWord(licensePlate = "1s3 456", words = ["looks", "pest", "stew", "show"])

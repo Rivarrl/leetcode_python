202ce87174fb05b00cd493377e1a356e273111cc
Rivarrl <1049793871@qq.com>
2019-09-24 00:28:02 +0800

    add

--- leetcode/simple5.py
+++ leetcode/simple5.py
@@ -849,8 +849,51 @@ def maxNumberOfBalloons(text):
         j += 1
 
 
+def findMode(root):
+    """
+    501. 二叉搜索树中的众数
+    给定一个有相同值的二叉搜索树（BST），找出 BST 中的所有众数（出现频率最高的元素）。
+    假定 BST 有如下定义：
+    结点左子树中所含结点的值小于等于当前结点的值
+    结点右子树中所含结点的值大于等于当前结点的值
+    左子树和右子树都是二叉搜索树
+    例如：
+    给定 BST [1,null,2,2],
+       1
+        \
+         2
+        /
+       2
+    返回[2].
+    提示：如果众数超过1个，不需考虑输出顺序
+    进阶：你可以不使用额外的空间吗？（假设由递归产生的隐式调用栈的开销不被计算在内）
+    :param root: TreeNode
+    :return: List[int]
+    """
+    def helper(node, v):
+        nonlocal m, ans
+        res = 0
+        if node:
+            left = helper(node.left, node.val)
+            right = helper(node.right, node.val)
+            res = left + right
+            if node.val == v:
+                res += 1
+                if res > m:
+                    m = res
+                    ans = v
+        return res
+    m = 0
+    ans = -1
+    helper(root, root.val)
+    print(ans)
+    return ans
+
+
 if __name__ == '__main__':
-    maxNumberOfBalloons("nlaebolko")
+    x = construct_tree_node([1,null,2,2])
+    findMode(x)
+    # maxNumberOfBalloons("nlaebolko")
     # distanceBetweenBusStops([0,11,6,1,4,3], 0, 5)
     # dayOfTheWeek(9, 9, 2019)
     # shortestCompletingWord(licensePlate = "1s3 456", words = ["looks", "pest", "stew", "show"])

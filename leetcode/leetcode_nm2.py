63634ac1b9da1a3b2407c68494b17c2a38d604a6
Rivarrl <1049793871@qq.com>
2019-09-26 00:00:17 +0800

    add

--- leetcode/leetcode_nm2.py
+++ leetcode/leetcode_nm2.py
@@ -763,6 +763,26 @@ def rangeBitwiseAnd(m, n):
     return m << i
 
 
+def numTilePossibilities(tiles):
+    """
+    1079. 活字印刷
+    你有一套活字字模 tiles，其中每个字模上都刻有一个字母 tiles[i]。返回你可以印出的非空字母序列的数目。
+    示例 1：
+    输入："AAB"
+    输出：8
+    解释：可能的序列为 "A", "B", "AA", "AB", "BA", "AAB", "ABA", "BAA"。
+    示例 2：
+    输入："AAABBC"
+    输出：188
+    提示：
+    1 <= tiles.length <= 7
+    tiles 由大写英文字母组成
+    :param tiles: str
+    :return: int
+    """
+
+
+
 if __name__ == '__main__':
     A = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
     B = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]

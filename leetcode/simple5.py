9ec5824bb1b1c965f1dcddfc1821dcf5280983a2
Rivarrl <1049793871@qq.com>
2019-10-01 23:55:45 +0800

    add

--- leetcode/simple5.py
+++ leetcode/simple5.py
@@ -933,6 +933,44 @@ def backspaceCompare(S, T):
     return stk == stk2
 
 
+def findShortestSubArray(nums):
+    """
+    697. 数组的度
+    给定一个非空且只包含非负数的整数数组 nums, 数组的度的定义是指数组里任一元素出现频数的最大值。
+    你的任务是找到与 nums 拥有相同大小的度的最短连续子数组，返回其长度。
+    示例 1:
+    输入: [1, 2, 2, 3, 1]
+    输出: 2
+    解释:
+    输入数组的度是2，因为元素1和2的出现频数最大，均为2.
+    连续子数组里面拥有相同度的有如下所示:
+    [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+    最短连续子数组[2, 2]的长度为2，所以返回2.
+    示例 2:
+    输入: [1,2,2,3,1,4,2]
+    输出: 6
+    注意:
+    nums.length 在1到50,000区间范围内。
+    nums[i] 是一个在0到49,999范围内的整数。
+    :param nums: List[int]
+    :return: int
+    """
+    d = {}
+    c = {}
+    dg = 0
+    for i, x in enumerate(nums):
+        c[x] = c.get(x, 0) + 1
+        dg = max(dg, c[x])
+        idx = d.get(x, [i, i])
+        idx[1] = i
+        d[x] = idx
+    ans = float('inf')
+    for k, v in c.items():
+        if v == dg:
+            ans = min(ans, d[k][1] - d[k][0] + 1)
+    return ans
+
+
 if __name__ == '__main__':
     x = construct_tree_node([1,null,2,null,null,2])
     findMode(x)

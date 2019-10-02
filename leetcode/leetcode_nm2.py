36639e8a84b4169ad75ae06e3ba47bd96d176f05
Rivarrl <1049793871@qq.com>
2019-10-02 21:39:28 +0800

    add

--- leetcode/leetcode_nm2.py
+++ leetcode/leetcode_nm2.py
@@ -1073,6 +1073,69 @@ def numTilings(N):
     return dp[N]
 
 
+def soupServings(N):
+    """
+    808. 分汤
+    有 A 和 B 两种类型的汤。一开始每种类型的汤有 N 毫升。有四种分配操作：
+    提供 100ml 的汤A 和 0ml 的汤B。
+    提供 75ml 的汤A 和 25ml 的汤B。
+    提供 50ml 的汤A 和 50ml 的汤B。
+    提供 25ml 的汤A 和 75ml 的汤B。
+    当我们把汤分配给某人之后，汤就没有了。每个回合，我们将从四种概率同为0.25的操作中进行分配选择。如果汤的剩余量不足以完成某次操作，我们将尽可能分配。当两种类型的汤都分配完时，停止操作。
+    注意不存在先分配100 ml汤B的操作。
+    需要返回的值： 汤A先分配完的概率 + 汤A和汤B同时分配完的概率 / 2。
+    示例:
+    输入: N = 50
+    输出: 0.625
+    解释:
+    如果我们选择前两个操作，A将首先变为空。对于第三个操作，A和B会同时变为空。对于第四个操作，B将首先变为空。
+    所以A变为空的总概率加上A和B同时变为空的概率的一半是 0.25 *(1 + 1 + 0.5 + 0)= 0.625。
+    注释:
+    0 <= N <= 10^9。
+    返回值在 10^-6 的范围将被认为是正确的。
+    :param N: int
+    :return: float
+    """
+    """
+    # 带备忘的dfs
+    def dfs(x, y):
+        if (x, y) in rec:
+            return rec[(x, y)]
+        ans = 0
+        if x <= 0 and y > 0:
+            ans = 1
+        elif x <= 0 and y <= 0:
+            ans = 0.5
+        elif x > 0 and y <= 0:
+            ans = 0
+        else:
+            ans = 0.25 * (dfs(x-100, y) + dfs(x-75, y-25) + dfs(x-50, y-50) + dfs(x-25, y-75))
+        rec[(x, y)] = ans
+        return ans
+    if N > 4800: return 1.0
+    rec = dict()
+    return dfs(N, N)
+    """
+    # dp
+    def fix(i):
+        return max(i, 0)
+    if N >= 4800: return 1.0
+    n = N//25
+    if N % 25 > 0:
+        n += 1
+    dp = [[0] * (n+1) for _ in range(n+1)]
+    dp[0][0] = 0.5
+    for i in range(1, n+1):
+        dp[i][0] = 0
+        dp[0][i] = 1
+    for i in range(1, n+1):
+        a1, a2, a3, a4 = fix(i-4), fix(i-3), fix(i-2), fix(i-1)
+        for j in range(1, n+1):
+            b1, b2, b3, b4 = fix(j), fix(j-1), fix(j-2), fix(j-3)
+            dp[i][j] = 0.25 * (dp[a1][b1] + dp[a2][b2] + dp[a3][b3] + dp[a4][b4])
+    return dp[n][n]
+
+
 if __name__ == '__main__':
     addNegabinary([1,0,1], [1,0,1])
     # numTilePossibilities("AAB")

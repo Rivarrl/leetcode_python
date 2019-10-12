1a789af0eca79ac7e7f1af5869d56ad0f05ca6f9
Rivarrl <1049793871@qq.com>
2019-10-12 17:07:06 +0800

    add

--- leetcode/leetcode_hr.py
+++ leetcode/leetcode_hr.py
@@ -1011,8 +1011,39 @@ def superEggDrop(K, N):
     return N
 
 
+def orderlyQueue(S, K):
+    """
+    899. 有序队列
+    给出了一个由小写字母组成的字符串 S。然后，我们可以进行任意次数的移动。
+    在每次移动中，我们选择前 K 个字母中的一个（从左侧开始），将其从原位置移除，并放置在字符串的末尾。
+    返回我们在任意次数的移动之后可以拥有的按字典顺序排列的最小字符串。
+    示例 1：
+    输入：S = "cba", K = 1
+    输出："acb"
+    解释：
+    在第一步中，我们将第一个字符（“c”）移动到最后，获得字符串 “bac”。
+    在第二步中，我们将第一个字符（“b”）移动到最后，获得最终结果 “acb”。
+    示例 2：
+    输入：S = "baaca", K = 3
+    输出："aaabc"
+    解释：
+    在第一步中，我们将第一个字符（“b”）移动到最后，获得字符串 “aacab”。
+    在第二步中，我们将第三个字符（“c”）移动到最后，获得最终结果 “aaabc”。
+    提示：
+    1 <= K <= S.length <= 1000
+    S 只由小写字母组成。
+    :param S: str
+    :param K: int
+    :return: str
+    """
+    if K == 1:
+        return min(S[i:] + S[:i] for i in range(len(S)))
+    return "".join(sorted(S))
+
+
 if __name__ == '__main__':
-    findNumOfValidWords(["aaaa","asas","able","ability","actt","actor","access"],["aboveyz","abrodyz","abslute","absoryz","actresz","gaswxyz"])
+    orderlyQueue("kuh", 1)
+    # findNumOfValidWords(["aaaa","asas","able","ability","actt","actor","access"],["aboveyz","abrodyz","abslute","absoryz","actresz","gaswxyz"])
     # maxProfit(2, [6,1,3,2,4,7])
     # shortestPalindrome("aacecaaa")
     # makeArrayIncreasing([1,5,3,6,7], [4,3,1])

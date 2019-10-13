7aa8e122f8f55b021f32e1b6e07eedcec581c4a8
Rivarrl <1049793871@qq.com>
2019-10-13 16:06:46 +0800

    add

--- leetcode/simple6.py
+++ leetcode/simple6.py
@@ -350,6 +350,46 @@ def judgeSquareSum(c):
     return c % 4 == 1
 
 
+def findUnsortedSubarray(nums):
+    """
+    581. 最短无序连续子数组
+    给定一个整数数组，你需要寻找一个连续的子数组，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序。
+    你找到的子数组应是最短的，请输出它的长度。
+    示例 1:
+    输入: [2, 6, 4, 8, 10, 9, 15]
+    输出: 5
+    解释: 你只需要对 [6, 4, 8, 10, 9] 进行升序排序，那么整个表都会变为升序排序。
+    说明 :
+    输入的数组长度范围在 [1, 10,000]。
+    输入的数组可能包含重复元素 ，所以升序的意思是<=。
+    :param nums: List[int]
+    :return: int
+    """
+    """
+    # 排序 + 双指针
+    a = sorted(nums)
+    p, q = 0, len(a) - 1
+    while p <= q and a[p] == nums[p]:
+        p += 1
+    while q >= p and a[q] == nums[q]:
+        q -= 1
+    return q - p + 1
+    """
+    # 单调栈
+    stk1, stk2 = [], []
+    p, q = len(nums) - 1, 0
+    for i in range(len(nums)):
+        while stk1 and nums[i] < nums[stk1[-1]]:
+            p = min(stk1.pop(), p)
+        stk1.append(i)
+    if len(stk1) == len(nums): return 0
+    for i in range(len(nums)-1, -1, -1):
+        while stk2 and nums[i] > nums[stk2[-1]]:
+            q = max(stk2.pop(), q)
+        stk2.append(i)
+    return q - p + 1
+
+
 if __name__ == '__main__':
     isMonotonic([6,5,4,4])
     # x = [26,2,16,16,5,5,26,2,5,20,20,5,2,20,2,2,20,2,16,20,16,17,16,2,16,20,26,16]

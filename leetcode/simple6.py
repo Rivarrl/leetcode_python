d18f00c939c471085650b453a1fbc7474c52652e
Rivarrl <1049793871@qq.com>
2019-10-17 00:08:45 +0800

    add

--- leetcode/simple6.py
+++ leetcode/simple6.py
@@ -409,6 +409,47 @@ def maximumProduct(nums):
     nums.sort()
     return max(nums[-1] * nums[-2] * nums[-3], nums[0] * nums[1] * nums[-1])
 
+
+def findRestaurant(list1, list2):
+    """
+    599. 两个列表的最小索引总和
+    假设Andy和Doris想在晚餐时选择一家餐厅，并且他们都有一个表示最喜爱餐厅的列表，每个餐厅的名字用字符串表示。
+    你需要帮助他们用最少的索引和找出他们共同喜爱的餐厅。 如果答案不止一个，则输出所有答案并且不考虑顺序。 你可以假设总是存在一个答案。
+    示例 1:
+    输入:
+    ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+    ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+    输出: ["Shogun"]
+    解释: 他们唯一共同喜爱的餐厅是“Shogun”。
+    示例 2:
+    输入:
+    ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+    ["KFC", "Shogun", "Burger King"]
+    输出: ["Shogun"]
+    解释: 他们共同喜爱且具有最小索引和的餐厅是“Shogun”，它有最小的索引和1(0+1)。
+    提示:
+    两个列表的长度范围都在 [1, 1000]内。
+    两个列表中的字符串的长度将在[1，30]的范围内。
+    下标从0开始，到列表的长度减1。
+    两个列表都没有重复的元素。
+    :param list1: List[str]
+    :param list2: List[str]
+    :return: List[str]
+    """
+    s2 = set(list2)
+    d = {}
+    m = float('inf')
+    for i, e in enumerate(list1):
+        if e in s2:
+            d[e] = i + list2.index(e)
+            m = min(m, d[e])
+    res = []
+    for k, v in d.items():
+        if v == m:
+            res.append(k)
+    return res
+
+
 if __name__ == '__main__':
     maximumProduct([1,2,3,4])
     # isMonotonic([6,5,4,4])

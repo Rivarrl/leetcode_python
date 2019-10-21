d8f8d26121a550258ee6d620eb1ebdb845552799
Rivarrl <1049793871@qq.com>
2019-10-21 22:29:35 +0800

    add

--- leetcode/simple6.py
+++ leetcode/simple6.py
@@ -512,6 +512,39 @@ def checkStraightLine(coordinates: List[List[int]]) -> bool:
     return True
 
 
+def balancedStringSplit(s: str) -> int:
+    """
+    1221. 分割平衡字符串
+    在一个「平衡字符串」中，'L' 和 'R' 字符的数量是相同的。
+    给出一个平衡字符串 s，请你将它分割成尽可能多的平衡字符串。
+    返回可以通过分割得到的平衡字符串的最大数量。
+    示例 1：
+    输入：s = "RLRRLLRLRL"
+    输出：4
+    解释：s 可以分割为 "RL", "RRLL", "RL", "RL", 每个子字符串中都包含相同数量的 'L' 和 'R'。
+    示例 2：
+    输入：s = "RLLLLRRRLR"
+    输出：3
+    解释：s 可以分割为 "RL", "LLLRRR", "LR", 每个子字符串中都包含相同数量的 'L' 和 'R'。
+    示例 3：
+    输入：s = "LLLLRRRR"
+    输出：1
+    解释：s 只能保持原样 "LLLLRRRR".
+    提示：
+    1 <= s.length <= 1000
+    s[i] = 'L' 或 'R'
+    :param s: str
+    :return: int
+    """
+    ctr, res = [0, 0], 0
+    n = len(s)
+    for i in range(n):
+        ctr['RL'.index(s[i])] += 1
+        if ctr[0] == ctr[1]:
+            res += 1
+    return res
+
+
 if __name__ == '__main__':
     checkStraightLine([[-1, 1], [-6, -4], [-6, 2], [2, 0], [-1, -2], [0, -4]])
     # maximumProduct([1,2,3,4])

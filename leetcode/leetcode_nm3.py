dad36bbd7d781719f077dde3ead225dc682ce271
Rivarrl <1049793871@qq.com>
2019-10-22 13:13:08 +0800

    add

--- leetcode/leetcode_nm3.py
+++ leetcode/leetcode_nm3.py
@@ -779,8 +779,38 @@ def balancedString(s: str) -> int:
     return r - l + 1
 
 
+def longestSubsequence(arr: List[int], difference: int) -> int:
+    """
+    1218. 最长定差子序列
+    给你一个整数数组 arr 和一个整数 difference，请你找出 arr 中所有相邻元素之间的差等于给定 difference 的等差子序列，并返回其中最长的等差子序列的长度。
+    示例 1：
+    输入：arr = [1,2,3,4], difference = 1
+    输出：4
+    解释：最长的等差子序列是 [1,2,3,4]。
+    示例 2：
+    输入：arr = [1,3,5,7], difference = 1
+    输出：1
+    解释：最长的等差子序列是任意单个元素。
+    示例 3：
+    输入：arr = [1,5,7,8,5,3,4,2,1], difference = -2
+    输出：4
+    解释：最长的等差子序列是 [7,5,3,1]。
+    提示：
+    1 <= arr.length <= 10^5
+    -10^4 <= arr[i], difference <= 10^4
+    """
+    d = {}
+    for e in arr:
+        if not e in d:
+            d[e] = int(bool(difference))
+        if e - difference in d:
+            d[e] = max(d[e], d[e-difference] + 1)
+    return max(d.values())
+
+
 if __name__ == '__main__':
-    removeSubfolders(["/a/b/c","/a/b/d","/a/b/ca","/a/b/d/c"])
+    longestSubsequence([4,12,10,0,-2,7,-8,9,-9,-12,-12,8,8], 0)
+    # removeSubfolders(["/a/b/c","/a/b/d","/a/b/ca","/a/b/d/c"])
     # res = probabilityOfHeads([0.5], 0)
     # print(res)
     # res = minAvailableDuration([[10,50],[60,120],[140,210]], [[0,15],[60,70]], 8)

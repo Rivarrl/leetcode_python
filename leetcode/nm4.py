008be821e475daa3e602476787600baf494a9ff6
Rivarrl <1049793871@qq.com>
2019-10-24 21:18:05 +0800

    add

---
+++ leetcode/nm4.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# ======================================
+# @File    : nm4.py
+# @Time    : 2019/10/24 16:32
+# @Author  : Rivarrl
+# ======================================
+from typing import List
+
+from algorithm_utils import *
+
+def numRollsToTarget(d: int, f: int, target: int) -> int:
+    """
+    1155. 掷骰子的N种方法
+    这里有 d 个一样的骰子，每个骰子上都有 f 个面，分别标号为 1, 2, ..., f。
+    我们约定：掷骰子的得到总点数为各骰子面朝上的数字的总和。
+    如果需要掷出的总点数为 target，请你计算出有多少种不同的组合情况（所有的组合情况总共有 f^d 种），模 10^9 + 7 后返回。
+    示例 1：
+    输入：d = 1, f = 6, target = 3
+    输出：1
+    示例 2：
+    输入：d = 2, f = 6, target = 7
+    输出：6
+    示例 3：
+    输入：d = 2, f = 5, target = 10
+    输出：1
+    示例 4：
+    输入：d = 1, f = 2, target = 3
+    输出：0
+    示例 5：
+    输入：d = 30, f = 30, target = 500
+    输出：222616187
+    提示：
+    1 <= d, f <= 30
+    1 <= target <= 1000
+    """
+    if d * f < target: return 0
+    mod = 10 ** 9 + 7
+    dp = [[0] * (max(f, target)+1) for _ in range(d+1)]
+    for i in range(1, f+1):
+        dp[1][i] = 1
+    for i in range(1, d):
+        for j in range(i, target):
+            for k in range(1, f+1):
+                if j + k <= target:
+                    dp[i+1][j+k] += dp[i][j]
+    return dp[d][target] % mod
+
+
+def dieSimulator(n: int, rollMax: List[int]) -> int:
+    """
+    1223. 掷骰子模拟
+    有一个骰子模拟器会每次投掷的时候生成一个 1 到 6 的随机数。
+    不过我们在使用它时有个约束，就是使得投掷骰子时，连续 掷出数字 i 的次数不能超过 rollMax[i]（i 从 1 开始编号）。
+    现在，给你一个整数数组 rollMax 和一个整数 n，请你来计算掷 n 次骰子可得到的不同点数序列的数量。
+    假如两个序列中至少存在一个元素不同，就认为这两个序列是不同的。由于答案可能很大，所以请返回 模 10^9 + 7 之后的结果。
+    示例 1：
+    输入：n = 2, rollMax = [1,1,2,2,2,3]
+    输出：34
+    解释：我们掷 2 次骰子，如果没有约束的话，共有 6 * 6 = 36 种可能的组合。但是根据 rollMax 数组，数字 1 和 2 最多连续出现一次，所以不会出现序列 (1,1) 和 (2,2)。因此，最终答案是 36-2 = 34。
+    示例 2：
+    输入：n = 2, rollMax = [1,1,1,1,1,1]
+    输出：30
+    示例 3：
+    输入：n = 3, rollMax = [1,1,1,2,2,3]
+    输出：181
+    提示：
+    1 <= n <= 5000
+    rollMax.length == 6
+    1 <= rollMax[i] <= 15
+    """
+    dp = [[[0] * 16 for _ in range(6)] for _ in range(n+1)]
+    mod = 10 ** 9 + 7
+    for j in range(6):
+        dp[1][j][0] = dp[1][j][1] = 1
+    for i in range(2, n+1):
+        for j in range(6):
+            g = min(i, rollMax[j])
+            dp[i][j][1] = sum(dp[i-1][x][0] for x in range(6) if x != j) % mod
+            for k in range(2, g+1):
+                dp[i][j][k] = dp[i-1][j][k-1]
+            dp[i][j][0] = sum(dp[i][j][x] for x in range(1, g+1)) % mod
+    res = sum(dp[n][j][0] for j in range(6))
+    print(res)
+    return res % mod
+
+
+if __name__ == '__main__':
+    dieSimulator(3, [1,1,1,2,2,3])
+    # x = numRollsToTarget(30,30,500)
+    # print(x)
+    pass
--- leetcode/simple6.py
+++ leetcode/simple6.py
@@ -611,8 +611,43 @@ def minimumAbsDifference(arr: List[int]) -> List[List[int]]:
     return [[e, e+d] for e in sorted(res)]
 
 
+def numSmallerByFrequency(queries: List[str], words: List[str]) -> List[int]:
+    """
+    1170. 比较字符串最小字母出现频次
+    我们来定义一个函数 f(s)，其中传入参数 s 是一个非空字符串；该函数的功能是统计 s  中（按字典序比较）最小字母的出现频次。
+    例如，若 s = "dcce"，那么 f(s) = 2，因为最小的字母是 "c"，它出现了 2 次。
+    现在，给你两个字符串数组待查表 queries 和词汇表 words，请你返回一个整数数组 answer 作为答案，其中每个 answer[i] 是满足 f(queries[i]) < f(W) 的词的数目，W 是词汇表 words 中的词。
+    示例 1：
+    输入：queries = ["cbd"], words = ["zaaaz"]
+    输出：[1]
+    解释：查询 f("cbd") = 1，而 f("zaaaz") = 3 所以 f("cbd") < f("zaaaz")。
+    示例 2：
+    输入：queries = ["bbb","cc"], words = ["a","aa","aaa","aaaa"]
+    输出：[1,2]
+    解释：第一个查询 f("bbb") < f("aaaa")，第二个查询 f("aaa") 和 f("aaaa") 都 > f("cc")。
+    提示：
+    1 <= queries.length <= 2000
+    1 <= words.length <= 2000
+    1 <= queries[i].length, words[i].length <= 10
+    queries[i][j], words[i][j] 都是小写英文字母
+    """
+    def f(s):
+        for i in range(26):
+            c = s.count(chr(ord('a') + i))
+            if c > 0: return c
+    cmp = [0] * 11
+    for w in words:
+        cmp[f(w)-1] += 1
+    for i in range(9, -1, -1):
+        cmp[i] += cmp[i+1]
+    res = [0] * len(queries)
+    for i, s in enumerate(queries):
+        res[i] = cmp[f(s)]
+    return res
+
 if __name__ == '__main__':
-    minCostToMoveChips([2,3,3,3,3])
+    numSmallerByFrequency(queries = ["bbb","cc"], words = ["a","aa","aaa","aaaa"])
+    # minCostToMoveChips([2,3,3,3,3])
     # checkStraightLine([[-1, 1], [-6, -4], [-6, 2], [2, 0], [-1, -2], [0, -4]])
     # maximumProduct([1,2,3,4])
     # isMonotonic([6,5,4,4])

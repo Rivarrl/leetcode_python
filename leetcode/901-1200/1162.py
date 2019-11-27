67eb5de2ad9338588f396620c5ee6e57e3713b94
rivarrl <1049793871@qq.com>
2019-11-27 19:44:28 +0800

    add

---
+++ leetcode/901-1200/1162.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# ======================================
+# @File    : 1162.py
+# @Time    : 2019/11/27 14:33
+# @Author  : Rivarrl
+# ======================================
+from algorithm_utils import *
+
+class Solution:
+    """
+    [1162. 地图分析](https://leetcode-cn.com/problems/as-far-from-land-as-possible/)
+    """
+    def maxDistance(self, grid: List[List[int]]) -> int:

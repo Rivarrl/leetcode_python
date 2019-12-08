e8e6bf267bb6de63c3a7ad31b306b003a101f468
rivarrl <1049793871@qq.com>
2019-12-08 19:12:30 +0800

    add: 12/8

--- leetcode/LCP/5282.py
+++ leetcode/LCP/5282.py
@@ -42,9 +42,43 @@ class Solution:
                         stk.insert(0, (nst, step+1))
         return -1
 
+
+    @timeit
+    def minFlips2(self, mat: List[List[int]]) -> int:
+        # 看行/列，选一个短的，这里最大只有3，行列无所谓。把第一行的所有状态穷举出来，用位压缩。
+        n, m = len(mat), len(mat[0])
+        dxy = (0, 0), (1, 0), (0, 1), (-1, 0), (0, -1)
+        res = inv = n * m + 1
+        for state in range(1 << n):
+            step = 0
+            tmp = [[0] * m for _ in range(n)]
+            for j in range(m):
+                if (state >> j) & 1:
+                    step += 1
+                    for dx, dy in dxy:
+                        x, y = dx, j + dy
+                        if 0 <= x < n and 0 <= y < m:
+                            tmp[x][y] ^= 1
+            for i in range(1, n):
+                for j in range(m):
+                    # 需要翻
+                    if tmp[i-1][j] ^ mat[i-1][j]:
+                        step += 1
+                        for dx, dy in dxy:
+                            x, y = i + dx, j + dy
+                            if 0 <= x < n and 0 <= y < m:
+                                tmp[x][y] ^= 1
+            for j in range(m):
+                if tmp[n-1][j] ^ mat[n-1][j]:
+                    break
+            else:
+                res = min(res, step)
+        return res == inv and -1 or res
+
+
 if __name__ == '__main__':
     a = Solution()
-    a.minFlips([[0,0],[0,1]])
-    a.minFlips([[0]])
-    a.minFlips([[1,1,1],[1,0,1],[0,0,0]])
-    a.minFlips([[1,0,0],[1,0,0]])
+    a.minFlips2([[0,0],[0,1]])
+    a.minFlips2([[0]])
+    a.minFlips2([[1,1,1],[1,0,1],[0,0,0]])
+    a.minFlips2([[1,0,0],[1,0,0]])

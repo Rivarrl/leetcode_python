eb6748715db194840bc4d229f54b35e1af92bfc4
rivarrl <1049793871@qq.com>
2019-12-17 00:46:03 +0800

    add: 12/17

---
+++ leetcode/301-600/331.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# ======================================
+# @File    : 331.py
+# @Time    : 2019/12/17 0:10
+# @Author  : Rivarrl
+# ======================================
+from algorithm_utils import *
+
+class Solution:
+    """
+    [331. 验证二叉树的前序序列化](https://leetcode-cn.com/problems/verify-preorder-serialization-of-a-binary-tree/)
+    """
+    def isValidSerialization(self, preorder: str) -> bool:
+
+if __name__ == '__main__':
+    a = Solution()
+    a.isValidSerialization()

c5109299fa33d778893fea8e3afc1d1391869453
rivarrl <1049793871@qq.com>
2019-12-20 02:27:30 +0800

    add: 12/20

--- leetcode/301-600/324.py
+++ leetcode/301-600/324.py
@@ -12,8 +12,17 @@ class Solution:
     """
     def wiggleSort(self, nums: List[int]) -> None:
         """
-
+        nlogn，先排序再按位置分配，分配的时候注意先小后大，如果按序分配，假如是123334，就变成了132334，而答案应该是341323，观察可得倒序分配可避免此问题.
         """
+        nums.sort()
+        n = len(nums[::2])
+        nums[::2], nums[1::2] = nums[:n][::-1], nums[n:][::-1]
+
+    def wiggleSort2(self, nums: List[int]) -> None:
+        """
+        类似三路快速排序(荷兰国旗算法)的方法，先找中位数，按中位数上下浮动去找中位数附近点分别放在2i和2i+1位置
+        """
+
 
 if __name__ == "__main__":
     a = Solution()
--- leetcode/601-900/741.py
+++ leetcode/601-900/741.py
@@ -11,39 +11,79 @@ class Solution:
     [741. 摘樱桃](https://leetcode-cn.com/problems/cherry-pickup/)
     """
     @timeit
+    def cherryPickup3(self, grid: List[List[int]]) -> int:
+        """
+        可看成两个人同时从0，0点摘，走到n-1,n-1的时候最多可摘多少，自顶向下。
+        """
+        from functools import lru_cache
+        @lru_cache(None)
+        def dp(x1, x2, k):
+            y1, y2 = k - x1, k - x2
+            if x1 == x2 == k == 0: return grid[0][0]
+            if any(e < 0 for e in {x1, x2, y1, y2}): return -1
+            if grid[x1][y1] == -1 or grid[x2][y2] == -1: return -1
+            m = max(dp(x1-1, x2, k-1), dp(x1, x2-1, k-1), dp(x1-1, x2-1, k-1), dp(x1, x2, k-1))
+            if m < 0: return -1
+            return m + grid[x1][y1] if x1 == x2 else m + grid[x1][y1] + grid[x2][y2]
+
+        N = len(grid)
+        return max(0, dp(N-1, N-1, (N-1) << 1))
+
+
+    @timeit
     def cherryPickup(self, grid: List[List[int]]) -> int:
         """
-        可看成两个人同时从0，0点摘，走到n-1,n-1的时候最多可摘多少。
+        自底向上
         """
-        stk = [(0, 0, 0, 0)]
         N = len(grid)
-        steps = N * 2 - 2
-        vis = [set() for _ in range(steps)]
-        vis[0].add((0, 0))
-        dp = [0] * (steps)
-        dxy = [[-1,0],[0,1],[1,0],[0,-1]]
-        while stk:
-            i1, i2, step, picked = stk.pop()
-            j1, j2 = step - i1, step - i2
-            if i1 == i2:
-                picked += grid[i1][j1]
-            else:
-                picked += grid[i1][j1] + grid[i2][j2]
-            if step == steps: continue
-            dp[step] = max(dp[step], picked)
-            for dx1, dy1 in dxy:
-                x1, y1 = i1 + dx1, j1 + dy1
-                if x1 < 0 or x1 == N or y1 < 0 or y1 == N: continue
-                for dx2, dy2 in dxy:
-                    x2, y2 = i2 + dx2, j2 + dy2
-                    if x2 < 0 or x2 == N or y2 < 0 or y2 == N or grid[x1][x2] == -1 or (x1, x2) in vis[step+1]: continue
-                    vis[step+1].add((x1, x2))
-                    stk.insert(0, (x1, x2, step+1, picked))
-        return dp[-1]
+        dp = [[-1] * N for _ in range(N)]
+        dp[0][0] = grid[0][0]
+        for k in range(1, (N << 1) - 1):
+            dp2 = [[-1] * N for _ in range(N)]
+            for x1 in range(max(0, k - N + 1), min(k+1, N)):
+                y1 = k - x1
+                if grid[x1][y1] == -1: continue
+                for x2 in range(max(0, k - N + 1), min(k+1, N)):
+                    y2 = k - x2
+                    if grid[x2][y2] == -1: continue
+                    cur = max(dp[xi][xj] for xi in (x1, x1-1) for xj in (x2, x2-1) if x1 >= 0 and xj >= 0)
+                    if cur >= 0:
+                        cur += grid[x1][y1]
+                        if x1 != x2: cur += grid[x2][y2]
+                    dp2[x1][x2] = cur
+            dp = dp2[:]
+        return max(0, dp[-1][-1])
 
 
 if __name__ == '__main__':
     a = Solution()
+    a.cherryPickup([[1,1,-1],
+                    [1,-1,1],
+                    [-1,1,1]])
     a.cherryPickup([[0, 1, -1],
                     [1, 0, -1],
                     [1, 1,  1]])
+    a.cherryPickup([[0, 1,1 ,0,0],
+                    [1, 1,1 ,1,0],
+                    [-1,1,1 ,1,-1],
+                    [0, 1,1 ,1,0],
+                    [1, 0,-1,0,0]])
+
+    a.cherryPickup([[1,1,1,1,0,0,0],
+                    [0,0,0,1,0,0,0],
+                    [0,0,0,1,0,0,1],
+                    [1,0,0,1,0,0,0],
+                    [0,0,0,1,0,0,0],
+                    [0,0,0,1,0,0,0],
+                    [0,0,0,1,1,1,1]])
+
+    a.cherryPickup([[1,-1,1,-1,1,1,1,1,1,-1],
+                    [-1,1,1,-1,-1,1,1,1,1,1],
+                    [1,1,1,-1,1,1,1,1,1,1],
+                    [1,1,1,1,1,1,1,1,1,1],
+                    [-1,1,1,1,1,1,1,1,1,1],
+                    [1,-1,1,1,1,1,-1,1,1,1],
+                    [1,1,1,-1,1,1,-1,1,1,1],
+                    [1,-1,1,-1,-1,1,1,1,1,1],
+                    [1,1,-1,-1,1,1,1,-1,1,-1],
+                    [1,1,-1,1,1,1,1,1,1,1]])

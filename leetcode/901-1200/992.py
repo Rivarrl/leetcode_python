0059a03c9eaf1febda293ed98fae5ee6d950a1ba
Rivarrl <1049793871@qq.com>
2019-12-23 14:29:22 +0800

    add: 12/23

---
+++ leetcode/901-1200/992.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# ======================================
+# @File    : 992.py
+# @Time    : 2019/12/23 12:23
+# @Author  : Rivarrl
+# ======================================
+from algorithm_utils import *
+
+class Solution:
+    @timeit
+    def minCameraCover(self, root: TreeNode) -> int:
+        def dfs(p):
+            if p
+            dfs(p.left)
+            dfs(p.right)
+
+if __name__ == '__main__':
+    a = Solution()
+    x = construct_tree_node([0,0,null,0,0])
+    a.minCameraCover(x)
+    x = construct_tree_node([0,0,null,0,null,0,null,null,0])
+    a.minCameraCover(x)

e3d20b117d870b9d5fee082e03b8a8cd3b6c95bf
rivarrl <1049793871@qq.com>
2020-03-11 00:34:50 +0800

    add: 3/11 打卡题

--- leetcode/901-1200/1013.py
+++ leetcode/901-1200/1013.py
@@ -26,9 +26,26 @@ class Solution:
             return False
         return dfs(0, 1)
 
+    @timeit
+    def canThreePartsEqualSum2(self, A: List[int]) -> bool:
+        s = sum(A)
+        n = len(A)
+        avg = s // 3
+        if avg * 3 != s: return False
+        pre = [0]
+        for i in range(n):
+            pre += [pre[-1] + A[i]]
+        pre = pre[1:]
+        if avg in pre:
+            i = pre.index(avg)
+            if 2 * avg in pre[i+1:-1]:
+                return True
+        return False
+
 
 if __name__ == '__main__':
     a = Solution()
-    a.canThreePartsEqualSum([0,2,1,-6,6,-7,9,1,2,0,1])
-    a.canThreePartsEqualSum([0,2,1,-6,6,7,9,-1,2,0,1])
-    a.canThreePartsEqualSum([3,3,6,5,-2,2,5,1,-9,4])
+    # a.canThreePartsEqualSum2([0,2,1,-6,6,-7,9,1,2,0,1])
+    # a.canThreePartsEqualSum2([0,2,1,-6,6,7,9,-1,2,0,1])
+    # a.canThreePartsEqualSum2([3,3,6,5,-2,2,5,1,-9,4])
+    a.canThreePartsEqualSum2([6,1,1,13,-1,0,-10,20])

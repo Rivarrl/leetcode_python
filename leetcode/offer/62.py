02a30906cbb1e2fb3fdf2b1e42c0d445400eadc8
rivarrl <1049793871@qq.com>
2020-03-13 22:40:33 +0800

    fix: 约瑟夫环

--- leetcode/offer/62.py
+++ leetcode/offer/62.py
@@ -10,6 +10,8 @@ from algorithm_utils import *
 class Solution:
     @timeit
     def lastRemaining(self, n: int, m: int) -> int:
+        # 模拟
+        # 时间O(N^2),空间O(N)
         arr = [i for i in range(n)]
         k = 0
         while len(arr) > 1:
@@ -17,8 +19,18 @@ class Solution:
             arr.pop(k)
         return arr[0]
 
+    @timeit
+    def lastRemaining2(self, n: int, m: int) -> int:
+        # 约瑟夫环问题，公式f(n) = (f(n-1)+m) % n
+        # 时间O(N), 空间O(1)
+        res = 0
+        for i in range(2, n+1):
+            res = (res + m) % i
+        return res
+
+
 if __name__ == '__main__':
     a = Solution()
-    a.lastRemaining(5, 3)
-    a.lastRemaining(10, 17)
-    a.lastRemaining(10**5, 10**6)
+    a.lastRemaining2(5, 3)
+    a.lastRemaining2(10, 17)
+    a.lastRemaining2(10**5, 10**6)

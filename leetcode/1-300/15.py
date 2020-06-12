377ef3203099ffa9ef568f03833ad411618f5430
Rivarrl <1049793871@qq.com>
2020-06-12 14:58:54 +0800

    add: 6/12

--- leetcode/1-300/15.py
+++ leetcode/1-300/15.py
@@ -12,16 +12,43 @@ class Solution:
     """
     @timeit
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        res = []
+        for a in range(n-2):
+            if a > 0 and nums[a] == nums[a-1]: continue
+            b, c = a + 1, n - 1
+            while b < c:
+                if nums[a] + nums[b] + nums[c] > 0:
+                    while b < c and nums[c] == nums[c-1]:
+                        c -= 1
+                    c -= 1
+                elif nums[a] + nums[b] + nums[c] < 0:
+                    while b < c and nums[b] == nums[b+1]:
+                        b += 1
+                    b += 1
+                else:
+                    res.append([nums[a], nums[b], nums[c]])
+                    while b < c and nums[c] == nums[c-1]:
+                        c -= 1
+                    c -= 1
+                    while b < c and nums[b] == nums[b+1]:
+                        b += 1
+                    b += 1
+        return res
+
+    @timeit
+    def threeSum2(self, nums: List[int]) -> List[List[int]]:
         res = []
         d = {}
         for x in nums:
             d[x] = d.get(x, 0) + 1
         nums = sorted(d.keys())
         n = len(nums)
-        for i, x in enumerate(nums):
-            if x < 0:
+        for i, a in enumerate(nums):
+            if a < 0:
                 # 分别求两个边界
-                target = -x
+                target = -a
                 lo, hi = i + 1, n
                 while lo < hi:
                     mi = lo + (hi - lo) // 2
@@ -38,22 +65,22 @@ class Solution:
                     else:
                         hi = mi
                 right = lo
-                for j in nums[left:right]:
-                    k = target - j
-                    if k in d and k != j:
-                        res.append([x, j, k])
-            if d[x] > 1:
-                if x == 0:
-                    if d[x] >= 3:
+                for b in nums[left:right]:
+                    c = target - b
+                    if c in d and c != b:
+                        res.append([a, b, c])
+            if d[a] > 1:
+                if a == 0:
+                    if d[a] >= 3:
                         res.append([0, 0, 0])
-                elif -2*x in d:
-                    if x > 0:
-                        res.append([-2*x, x, x])
+                elif -2*a in d:
+                    if a > 0:
+                        res.append([-2*a, a, a])
                     else:
-                        res.append([x, x, -2*x])
+                        res.append([a, a, -2*a])
         return res
 
 if __name__ == '__main__':
     a = Solution()
-    # a.threeSum([-1,0,1,2,-1,-4])
-    a.threeSum([6,-5,-6,-1,-2,8,-1,4,-10,-8,-10,-2,-4,-1,-8,-2,8,9,-5,-2,-8,-9,-3,-5])
+    a.threeSum([-1,0,1,2,-1,-4])
+    # a.threeSum2([6,-5,-6,-1,-2,8,-1,4,-10,-8,-10,-2,-4,-1,-8,-2,8,9,-5,-2,-8,-9,-3,-5])

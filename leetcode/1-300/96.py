5246c360527d26d32bb40497133ba0ac7278dba8
rivarrl <1049793871@qq.com>
2020-07-15 19:05:09 +0800

    add: 7/7

---
+++ leetcode/1-300/96.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# ======================================
+# @File    : 96.py
+# @Time    : 2020/7/15 1:17 下午
+# @Author  : Rivarrl
+# ======================================
+from algorithm_utils import *
+
+class Solution:
+    """
+    [96. 不同的二叉搜索树](https://leetcode-cn.com/problems/unique-binary-search-trees/)
+    """
+    @timeit
+    def numTrees(self, n: int) -> int:
+        # 卡塔兰数
+
+
+if __name__ == '__main__':
+    a = Solution()
+    a.numTrees(3)

683422eade87b7269e97fce35295852ed400bf52
Rivarrl <1049793871@qq.com>
2020-07-31 01:47:32 +0800

    add: 7/30 欠2题

--- leetcode/301-600/473.py
+++ leetcode/301-600/473.py
@@ -12,6 +12,26 @@ class Solution:
     """
     @timeit
     def makesquare(self, nums: List[int]) -> bool:
+        n = len(nums)
+        if n == 0: return False
+        s = sum(nums)
+        a = s // 4
+        if a * 4 < s: return False
+        nums.sort(reverse=True)
+        if nums[0] > a: return False
+        arr = [0] * 4
+        def f(i):
+            if i == n:
+                # 3条边满足即可
+                return all(e == a for e in arr[:-1])
+            for j in range(4):
+                if arr[j] + nums[i] <= a:
+                    arr[j] += nums[i]
+                    if f(i+1): return True
+                    arr[j] -= nums[i]
+            return False
+        return f(0)
+
 
 
 if __name__ == '__main__':

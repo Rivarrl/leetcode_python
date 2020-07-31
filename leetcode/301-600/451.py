c93d0d6906fb37813fda0933bc18151024cf7a96
rivarrl <1049793871@qq.com>
2020-07-31 18:54:41 +0800

    add: 7/31

--- leetcode/301-600/451.py
+++ leetcode/301-600/451.py
@@ -10,7 +10,18 @@ class Solution:
     """
     [451. 根据字符出现频率排序](https://leetcode-cn.com/problems/sort-characters-by-frequency/)
     """
+    @timeit
     def frequencySort(self, s: str) -> str:
+        from collections import Counter, defaultdict
+        d = Counter(s)
+        dr = defaultdict(list)
+        for k, v in d.items():
+            dr[v].append(k)
+        res = ''
+        for k in sorted(dr.keys(), reverse=True):
+            for v in dr[k]:
+                res += v * k
+        return res
 
 if __name__ == '__main__':
     a = Solution()

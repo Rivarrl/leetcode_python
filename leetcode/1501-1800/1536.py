b36b1e852d07cf62a0f134e09652fa429ca9674f
Rivarrl <1049793871@qq.com>
2020-08-07 03:58:23 +0800

    add: 8/7

--- leetcode/1501-1800/1536.py
+++ leetcode/1501-1800/1536.py
@@ -10,7 +10,32 @@ class Solution:
     """
     [1536. 排布二进制网格的最少交换次数](https://leetcode-cn.com/problems/minimum-swaps-to-arrange-a-binary-grid/)
     """
+    @timeit
     def minSwaps(self, grid: List[List[int]]) -> int:
+        # 统计从右向左数第一个1的位置，结果为按从小到大排序
+        n = len(grid)
+        arr = [0] * n
+        for i in range(n):
+            for j in range(n-1, -1, -1):
+                if grid[i][j] == 1:
+                    arr[i] = j
+                    break
+        res = 0
+        for i in range(n):
+            if arr[i] <= i: continue
+            k = 0
+            j = i+1
+            while j < n:
+                if arr[j] <= i:
+                    k = j
+                    break
+                j += 1
+            if j == n: return -1
+            res += (k - i)
+            while k > i:
+                arr[k], arr[k-1] = arr[k-1], arr[k]
+                k -= 1
+        return res
 
 
 if __name__ == '__main__':
--- leetcode/1501-1800/1537.py
+++ leetcode/1501-1800/1537.py
@@ -10,4 +10,38 @@ class Solution:
     """
     [1537. 最大得分](https://leetcode-cn.com/problems/get-the-maximum-score/)
     """
+    @timeit
     def maxSum(self, nums1: List[int], nums2: List[int]) -> int:
+        mod = 10 ** 9 + 7
+        n, m = len(nums1), len(nums2)
+        i = j = 0
+        r1 = r2 = 0
+        while i < n or j < m:
+            if i == n:
+                while j < m:
+                    r2 += nums2[j]
+                    j += 1
+            elif j == m:
+                while i < n:
+                    r1 += nums1[i]
+                    i += 1
+            elif nums1[i] < nums2[j]:
+                r1 += nums1[i]
+                i += 1
+            elif nums1[i] > nums2[j]:
+                r2 += nums2[j]
+                j += 1
+            else:
+                r1 = r2 = max(r1, r2) + nums1[i]
+                i += 1
+                j += 1
+        return max(r1, r2) % mod
+
+if __name__ == '__main__':
+    a = Solution()
+    a.maxSum(nums1 = [2,4,5,8,10], nums2 = [4,6,8,9])
+    a.maxSum(nums1 = [1,3,5,7,9], nums2 = [3,5,100])
+    a.maxSum(nums1 = [1,2,3,4,5], nums2 = [6,7,8,9,10])
+    a.maxSum(nums1 = [1,4,5,8,9,11,19], nums2 = [2,3,4,11,12])
+    a.maxSum([8,18,38,58,69,83,95,96,99,104,107,126,145,162,172,176,195,196,198,213,228,231,251,253,256,260,265,266,269,272,273,277,279,282,285,290,300,310],
+[7,22,25,27,30,40,57,61,66,78,83,87,91,104,115,132,135,140,144,163,182,187,201,203,206,209,219,224,241,254,259,261,266,283,295,296,300,315,318,322,334,346,357,377,382,383,401,405,410])

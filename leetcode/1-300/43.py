2c519c7c9becbd46656ca8f1ff50141bb94ae5f5
Rivarrl <1049793871@qq.com>
2020-08-13 00:47:49 +0800

    add: 8/13

---
+++ leetcode/1-300/43.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# ======================================
+# @File    : 43.py
+# @Time    : 2020/8/13 0:46
+# @Author  : Rivarrl
+# ======================================
+from algorithm_utils import *
+
+class Solution:
+    """
+    [43. 字符串相乘](https://leetcode-cn.com/problems/multiply-strings/)
+    """
+    @timeit
+    def multiply(self, num1: str, num2: str) -> str:
+
+
+if __name__ == '__main__':
+    a = Solution()
+    a.multiply("2", "3")
+    a.multiply("123", "456")

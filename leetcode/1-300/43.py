29dc9243cfd46c1db93389f11442ff777f9a27eb
rivarrl <1049793871@qq.com>
2020-08-13 14:04:47 +0800

    add: 8/13

--- leetcode/1-300/43.py
+++ leetcode/1-300/43.py
@@ -12,9 +12,26 @@ class Solution:
     """
     @timeit
     def multiply(self, num1: str, num2: str) -> str:
-
+        n1, n2 = len(num1), len(num2)
+        arr = [0] * (n1+n2)
+        for i in range(n1-1, -1, -1):
+            x = ord(num1[i]) - ord('0')
+            for j in range(n2-1, -1, -1):
+                y = ord(num2[j]) - ord('0')
+                arr[i+j] += (x * y) % 10
+                arr[i+j-1] += (x * y) // 10 + arr[i+j] // 10
+                arr[i+j] %= 10
+        if arr[-1] > 0:
+            arr = [arr[-1]] + arr[:-1]
+        else:
+            arr = arr[:-1]
+        while len(arr) > 1 and arr[0] == 0:
+            arr.pop(0)
+        return ''.join(chr(e+ord('0')) for e in arr)
 
 if __name__ == '__main__':
     a = Solution()
     a.multiply("2", "3")
     a.multiply("123", "456")
+    a.multiply("99", "999")
+    a.multiply("0", "9999")

80a4180734f9e41a6591631a47551087e5a39a71
rivarrl <1049793871@qq.com>
2020-08-14 18:01:39 +0800

    add: 8/14

--- leetcode/301-600/420.py
+++ leetcode/301-600/420.py
@@ -12,9 +12,35 @@ class Solution:
     """
     @timeit
     def strongPasswordChecker(self, s: str) -> int:
-        
+        n = len(s)
+        miss = [0] * 3
+        loc = []
+        for i in range(n):
+            if 9 >= ord(s[i]) - ord('0') >= 0:
+                miss[0] = 1
+            elif ord('a') <= ord(s[i]) <= ord('z'):
+                miss[1] = 1
+            else:
+                miss[2] = 1
+        sms = sum(miss)
+        if n < 6: return max(3 - sms, 6 - n)
+        p = c = 0
+        r = [0] * 2
+        for i in range(n):
+            if i > 0 and s[i] == s[i - 1]:
+                p += 1
+            else:
+                c += p // 3
+                if p >= 3 and p % 3 < 2:
+                    r[p%3] += 1
+                p = 1
+        print(r)
+        if n <= 20: return max(3 - sms, c)
+
 
 
 if __name__ == '__main__':
     a = Solution()
-    a.strongPasswordChecker()
+    # a.strongPasswordChecker("666")
+    # a.strongPasswordChecker("Tql333")
+    a.strongPasswordChecker("gG2222233544gpg3444rrrrrrr")

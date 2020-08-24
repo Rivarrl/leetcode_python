fb20a96b5924e8af40b0076fa6f32ec50d67b7d8
Rivarrl <1049793871@qq.com>
2020-08-25 00:14:55 +0800

    add: 8/25

--- leetcode/msjd/05-03.py
+++ leetcode/msjd/05-03.py
@@ -12,9 +12,19 @@ class Solution:
     """
     @timeit
     def reverseBits(self, num: int) -> int:
-
+        pre = cur = 0
+        res = 1
+        for i in range(32):
+            if num & (1 << i):
+                cur += 1
+            else:
+                res = max(res, pre + cur)
+                pre, cur = cur + 1, 0
+        res = max(res, cur+pre)
+        return res
 
 if __name__ == '__main__':
     a = Solution()
     a.reverseBits(1775)
     a.reverseBits(7)
+    a.reverseBits(2147483646)

b5b33d06a29dd71659e56deb4b19608b8f6318fe
Rivarrl <1049793871@qq.com>
2020-09-11 21:56:10 +0800

    add: 9/11

--- leetcode/1-300/216.py
+++ leetcode/1-300/216.py
@@ -7,6 +7,9 @@
 from algorithm_utils import *
 
 class Solution:
+    """
+    [216. 组合总和 III](https://leetcode-cn.com/problems/combination-sum-iii/)
+    """
     @timeit
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         if k > 9 or k < 1: return []
@@ -24,7 +27,27 @@ class Solution:
             return res
         return f(1, n, k)
 
+    @timeit
+    def combinationSum32(self, k: int, n: int) -> List[List[int]]:
+        if k > 9 or k < 1: return []
+        if n > 45 or n < 1: return []
+        def bit_count(mask):
+            res = 0
+            while mask:
+                mask &= (mask - 1)
+                res += 1
+            return res
+        res = []
+        for mask in range(1 << 9):
+            if bit_count(mask) == k:
+                cur = sum([i+1 for i in range(9) if mask & (1 << i)])
+                if cur == n:
+                    res.append([i+1 for i in range(9) if mask & (1 << i)])
+        return res
+
 if __name__ == '__main__':
     a = Solution()
     a.combinationSum3(3, 7)
     a.combinationSum3(3, 9)
+    a.combinationSum32(3, 7)
+    a.combinationSum32(3, 9)

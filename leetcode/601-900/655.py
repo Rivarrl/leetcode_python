7500f2c486622176ca925adaa91583fae31a7712
rivarrl <1049793871>
2020-10-10 13:54:59 +0800

    add: 10/10

--- leetcode/601-900/655.py
+++ leetcode/601-900/655.py
@@ -14,39 +14,37 @@ class Solution:
     @timeit
     def printTree(self, root: TreeNode) -> List[List[str]]:
         if not root: return []
-        stk = [[0, root]]
+        stk = [root]
         rec = []
         level = 0
-        while stk:
+        while stk and any(e is not None for e in stk):
             tmp = []
             rec.append(list())
-            while stk:
-                n, p = stk.pop()
-                rec[level].append([n, p.val])
-                if p.right:
-                    tmp.append([n*2+2, p.right])
-                if p.left:
-                    tmp.append([n*2+1, p.left])
+            for p in stk:
+                if p is not None:
+                    pv = str(p.val)
+                    tmp.append(p.left)
+                    tmp.append(p.right)
+                else:
+                    pv = ''
+                    tmp.extend([None, None])
+                rec[level].append(pv)
             stk = tmp[:]
             level += 1
         res = [[''] * ((1 << level) - 1) for _ in range(level)]
-        lv, last = -1, 0
         for i in range(level):
-            for j in range(1 << i, 1 << (i+1)):
-                if j - 1
-        for lv in range(level-1, -1, -1):
-            pre = (1 << (level - 1 - lv)) - 1
-            for i, v in rec[lv]:
-                res[lv][pre + i] = str(v)
-        matrix_pretty_print(res)
-        print(res)
+            j = (1 << (level - i - 1)) - 1
+            s = (1 << (level - i))
+            for r in rec[i]:
+                res[i][j] = r
+                j += s
         return res
 
 if __name__ == '__main__':
     a = Solution()
-    # x = construct_tree_node([1,2])
-    # a.printTree(x)
-    # x = construct_tree_node([1,2,3,null,4])
-    # a.printTree(x)
-    x = construct_tree_node([1,2,5,3,null,null,null,null,4])
+    x = construct_tree_node([1,2])
+    a.printTree(x)
+    x = construct_tree_node([1,2,3,null,4])
+    a.printTree(x)
+    x = construct_tree_node([1,2,5,3,null,null,null,4])
     a.printTree(x)

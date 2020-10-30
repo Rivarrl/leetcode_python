7d667ca9765eec5ab3efb16dbee306c05c1e5da8
rivarrl <1049793871>
2020-10-30 13:53:46 +0800

    add: 10/30 17-24 还不对

---
+++ leetcode/301-600/463.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# ======================================
+# @File    : 463.py
+# @Time    : 2020/10/30 1:10 下午
+# @Author  : Rivarrl
+# ======================================
+from algorithm_utils import *
+
+class Solution:
+    @timeit
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        if n == 0: return 0
+        m = len(grid[0])
+        res = 0
+        def lrud(i, j):
+            res = 0
+            if i > 0: res += grid[i-1][j]
+            if j > 0: res += grid[i][j-1]
+            if i < n-1: res += grid[i+1][j]
+            if j < m-1: res += grid[i][j+1]
+            return res
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]:
+                    res += 4 - lrud(i, j)
+        return res
+
+if __name__ == '__main__':
+    a = Solution()
+    a.islandPerimeter([[0,1,0,0],
+                     [1,1,1,0],
+                     [0,1,0,0],
+                     [1,1,0,0]])

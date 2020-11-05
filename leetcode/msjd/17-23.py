bba9a121e86028fbbfc8ab21569a507a1754fa2a
rivarrl <1049793871>
2020-11-05 22:30:11 +0800

    add: 11/5 17-24 还不对

--- leetcode/msjd/17-23.py
+++ leetcode/msjd/17-23.py
@@ -12,44 +12,37 @@ class Solution:
     """
     @timeit
     def findSquare(self, matrix: List[List[int]]) -> List[int]:
-        import heapq
+        from collections import defaultdict
+        if not matrix:
+            return []
+        row = defaultdict(int)
+        col = defaultdict(int)
         n = len(matrix)
-        pre = [[0] * (n+1) for _ in range(n+1)]
-        for i in range(n):
-            for j in range(n):
-                pre[i+1][j+1] += pre[i+1][j] + matrix[i][j]
-        for j in range(1, n+1):
-            for i in range(1, n+1):
-                pre[i][j] += pre[i-1][j]
-        q = [[-n, 0, 0]]
-        seen = set()
         res = []
-        matrix_pretty_print(pre)
-        while q:
-            rs, r, c = heapq.heappop(q)
-            s = -rs
-            x, y = r + s, c + s
-            print(r, c, s, x, y, pre[r][c], pre[x][y], pre[x][c], pre[r][y],
-                  pre[r][c] + pre[x][y] - pre[x][c] - pre[r][y])
-            if pre[r][c] + pre[x][y] - pre[x][c] - pre[r][y] == 0:
-                # if res and res[-1] > s: break
-                if not res or res[0] > r or (res[0] == r and res[1] > c):
-                    res = [r, c, s]
-            else:
-                for i, j in ((r, c), (r, c+1), (r+1, c), (r+1, c+1)):
-                    if s - 1 > 0 and (i, j, s-1) not in seen:
-                        seen.add((i,j,s-1))
-                        q.append([1-s,i,j])
+        for r in range(n-1, -1, -1):
+            for c in range(n-1, -1, -1):
+                if matrix[r][c] == 0:
+                    row[r, c] = 1 + row[r, c + 1]
+                    col[r, c] = 1 + col[r + 1, c]
+        for r in range(n):
+            for c in range(n):
+                if matrix[r][c] == 0:
+                    m = min(row[r, c], col[r, c])
+                    i = 0 if not res else res[2]
+                    for size in range(m, i, -1):
+                        if col[r, c + size - 1] >= size and row[r + size - 1, c] >= size:
+                            res = [r, c, size]
+                            break
         return res
 
 if __name__ == '__main__':
     a = Solution()
-    # a.findSquare([[1,0,1],
-    #               [0,0,1],
-    #               [0,0,1]])
-    # a.findSquare([[0,1,1],
-    #               [1,0,1],
-    #               [1,1,0]])
+    a.findSquare([[1,0,1],
+                  [0,0,1],
+                  [0,0,1]])
+    a.findSquare([[0,1,1],
+                  [1,0,1],
+                  [1,1,0]])
     a.findSquare([[1, 1, 1, 1, 0, 1, 0, 1, 1, 1],
                   [1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
                   [1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
@@ -60,3 +53,13 @@ if __name__ == '__main__':
                   [0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
                   [1, 0, 1, 0, 1, 1, 0, 1, 1, 1],
                   [1, 1, 1, 0, 1, 0, 0, 1, 1, 1]])
+    a.findSquare([[1, 1, 1, 0, 1, 1, 0, 1, 0, 0],
+                  [0, 1, 0, 1, 1, 0, 0, 0, 1, 1],
+                  [0, 0, 1, 1, 0, 0, 1, 1, 1, 0],
+                  [0, 1, 1, 1, 0, 1, 0, 0, 1, 0],
+                  [1, 1, 0, 1, 1, 0, 1, 0, 0, 1],
+                  [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+                  [1, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+                  [1, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+                  [1, 1, 1, 1, 0, 1, 0, 1, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 1, 1, 0]])

0e56aebe81e8886d50816e83fe8043c84cd4538c
rivarrl <1049793871>
2020-11-12 23:13:26 +0800

    add: 11/12

--- leetcode/msjd/17-24.py
+++ leetcode/msjd/17-24.py
@@ -13,12 +13,23 @@ class Solution:
     @timeit
     def getMaxMatrix(self, matrix: List[List[int]]) -> List[int]:
         res = [0] * 4
-        m = matrix[0][0]
+        max_all = matrix[0][0]
         n, m = len(matrix), len(matrix[0])
         for r1 in range(n):
             dp = [0] * m
             for r2 in range(r1, n):
-                dp[]
+                pre = min_pre = 0
+                min_pre_idx = -1
+                for c2 in range(m):
+                    dp[c2] += matrix[r2][c2]
+                    pre += dp[c2]
+                    if pre - min_pre > max_all:
+                        max_all = pre - min_pre
+                        res = [r1, min_pre_idx+1, r2, c2]
+                    if pre < min_pre:
+                        min_pre = pre
+                        min_pre_idx = c2
+        return res
 
 
 if __name__ == '__main__':

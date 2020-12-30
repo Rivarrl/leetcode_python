f103baf50f89463db7d2afb1b0db215c8e41d62a
rivarrl <1049793871>
2020-12-30 14:00:52 +0800

    add: 12/30

--- leetcode/601-900/749.py
+++ leetcode/601-900/749.py
@@ -12,7 +12,43 @@ class Solution:
     """
     @timeit
     def containVirus(self, grid: List[List[int]]) -> int:
+        n, m = len(grid), len(grid[0])
+        def f(i, j):
+            if (i, j) not in seen:
+                seen.add((i, j))
+                regions[-1].add((i, j))
+                for ni, nj in ((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)):
+                    if 0 <= ni < n and 0 <= nj < m:
+                        if grid[ni][nj] == 1:
+                            f(ni, nj)
+                        elif grid[ni][nj] == 0:
+                            infect[-1].add((ni, nj))
+                            blocks[-1] += 1
+        ans = 0
+        while True:
+            seen = set()
+            regions = [] # 当前感染区域
+            infect = [] # 下次感染扩散的block
+            blocks = [] # 需要建墙的数量
+            for i in range(n):
+                for j in range(m):
+                    if grid[i][j] == 1 and (i, j) not in seen:
+                        regions.append(set())
+                        infect.append(set())
+                        blocks.append(0)
+                        f(i, j)
+            if not regions: break
+            rescue_idx = infect.index(max(infect, key=len))
+            ans += blocks[rescue_idx]
 
+            for i in range(len(regions)):
+                if i == rescue_idx:
+                    for x, y in regions[i]:
+                        grid[x][y] = -1
+                else:
+                    for x, y in infect[i]:
+                        grid[x][y] = 1
+        return ans
 
 if __name__ == '__main__':
     a = Solution()

344ab8ea4d14102438ba89f9b71e2cd147098668
liutianbao <tianbao.liu@rjoy.com>
2021-01-13 20:01:31 +0800

    add: 1/11

--- leetcode/1201-1500/1203.py
+++ leetcode/1201-1500/1203.py
@@ -12,6 +12,60 @@ class Solution:
     """
     @timeit
     def sortItems(self, n: int, m: int, group: List[int], beforeItems: List[List[int]]) -> List[int]:
+        for i in range(n):
+            if group[i] == -1:
+                group[i] = m
+                m += 1
+        indegree_group = [0 for _ in range(m)] # 组间入度
+        indegree_item = [0 for _ in range(n)]
+        graph_group = [set() for _ in range(m)]
+        graph_item = [set() for _ in range(n)]
+        group_data = [set() for _ in range(m)]
+        # 存放组与项目的关系
+        for i in range(n):
+            group_data[group[i]].add(i)
+
+        work_group = [i for i in range(m) if len(group_data[i]) > 0]
+
+        for i in range(n):
+            for j in beforeItems[i]:
+                if group[i] == group[j]:
+                    # 属于组内图
+                    indegree_item[i] += 1
+                    graph_item[j].add(i)
+                else:
+                    # 属于组间图
+                    if not group[i] in graph_group[group[j]]:
+                        indegree_group[group[i]] += 1
+                        graph_group[group[j]].add(group[i])
+        q = [i for i in range(m) if indegree_group[i] == 0]
+        if len(q) == 0: return []
+        seq = []
+        while q:
+            p = q.pop()
+            seq.append(p)
+            for i in graph_group[p]:
+                indegree_group[i] -= 1
+                if indegree_group[i] == 0:
+                    q.insert(0, i)
+        if len(seq) < len(indegree_group): return []
+        for k in work_group:
+            q = [i for i in group_data[k] if indegree_item[i] == 0]
+            if len(q) == 0: return []
+            _seq = []
+            while q:
+                p = q.pop()
+                _seq.append(p)
+                for i in graph_item[p]:
+                    indegree_item[i] -= 1
+                    if indegree_item[i] == 0:
+                        q.insert(0, i)
+            if len(_seq) < len(group_data[k]): return []
+            group_data[k] = _seq
+        res = []
+        for i in range(m):
+            res += group_data[seq[i]]
+        return res
 
 
 if __name__ == '__main__':

7ebe71838838aaf9a9111781513ed562e129fcbb
rivarrl <1049793871@qq.com>
2021-02-23 18:31:59 +0800

    add: 2/23

---
+++ leetcode/1501-1800/1502.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# ======================================
+# @File    : 1502
+# @Time    : 2021/2/23 18:05
+# @Author  : Rivarrl
+# ======================================
+from algorithm_utils import *
+
+class Solution:
+    """
+    [1502. 判断能否形成等差数列](https://leetcode-cn.com/problems/can-make-arithmetic-progression-from-sequence/)
+    """
+    @timeit
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+
+
+if __name__ == '__main__':
+    a = Solution()
+    a.canMakeArithmeticProgression(arr = [3,5,1])
+    a.canMakeArithmeticProgression(arr = [1,2,4])
--- leetcode/1501-1800/1752.py
+++ leetcode/1501-1800/1752.py
@@ -7,4 +7,29 @@
 from algorithm_utils import *
 
 class Solution:
+    """
+    [1752. 检查数组是否经排序和轮转得到](https://leetcode-cn.com/problems/check-if-array-is-sorted-and-rotated/)
+    """
+    @timeit
     def check(self, nums: List[int]) -> bool:
+        n = len(nums)
+        i = nums.index(min(nums))
+        if i == 0 and nums[i] == nums[-1]:
+            j = n - 1
+            while j > i and nums[j] == nums[i]:
+                j -= 1
+            i = (j + 1) % n
+        k = (i + n - 1) % n
+        while i != k:
+            j = (i+1) % n
+            if nums[i] > nums[j]: return False
+            i = j
+        return True
+
+if __name__ == '__main__':
+    a = Solution()
+    a.check(nums = [3,4,5,1,2])
+    a.check(nums = [2,1,3,4])
+    a.check(nums = [1,2,3])
+    a.check(nums = [1,1,1])
+    a.check(nums = [6,10,6])

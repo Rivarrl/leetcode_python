fec1d59b42f5b3b774819b0022d9ab25c2a3766a
rivarrl <1049793871@qq.com>
2021-03-04 18:29:36 +0800

    add: 3/4

---
+++ leetcode/301-600/354.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# ======================================
+# @File    : 354
+# @Time    : 2021/3/4 13:13
+# @Author  : Rivarrl
+# ======================================
+from algorithm_utils import *
+
+class Solution:
+    """
+    [354. 俄罗斯套娃信封问题](https://leetcode-cn.com/problems/russian-doll-envelopes/)
+    """
+    @timeit
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+
+
+if __name__ == '__main__':
+    a = Solution()
+    a.maxEnvelopes(envelopes = [[5,4],[6,4],[6,7],[2,3]])
